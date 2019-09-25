cefef48307772e387d26c4f190a80f9273d44055
ali <aligng1620@gmail.com>
2019-09-25 08:55:37 -0300

    initial setup

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# ue-ran-sim
+## Kurulum ve Kullanım
+
+Projenin kök dizininde aşağıdaki adımlar sırayla uygulanır.
+
+### Pycrate Kurulumu
+- `git clone https://github.com/P1sec/pycrate.git`
+### ASN'den Kod Türetme
+- `python3 pycrate/tools/pycrate_asn1compile.py -i ngap-15-2-0/*.asn -o generated`
+### Çalıştırma
+- `python3 main.py`
---
+++ main.py
@@ -0,0 +1,51 @@
+from binascii import unhexlify, hexlify
+import generated
+
+InitiatingMessage = generated.NGAP_PDU_Descriptions.InitiatingMessage
+
+print(InitiatingMessage.get_proto())
+
+
+
+
+exit(0)
+
+
+b = unhexlify("00000400550002000500260021207e004171000d010011000000000099898877f71001002e04804080402f0201010079000f400001100000011000000110000075005a400118")
+InitialUEMessage = generated.NGAP_PDU_Contents.InitialUEMessage
+InitialUEMessage.from_aper(b)
+print(InitialUEMessage.to_json())
+
+exit(0)
+
+print(InitialUEMessage.get_proto())
+
+Value = {
+    "protocolIEs": [
+        {
+            "id": 85,
+            "criticality": "reject",
+            "value": ("RAN-UE-NGAP-ID", 5)
+        },
+        {
+            "id": 38,
+            "criticality": "reject",
+            "value": ("NAS-PDU", unhexlify("7e004171000d010011000000000099898877f71001002e04804080402f020101"))
+        },
+        #{
+        #    "id": 121,
+        #    "criticality": "reject",
+        #    "value": ("UserLocationInformation", unhexlify("400001100000011000000110000075"))
+        #}
+    ]
+}
+InitialUEMessage.set_val(Value)
+print(InitialUEMessage.to_asn1())
+print(hexlify(InitialUEMessage.to_aper()))
+
+
+#
+#b = unhexlify('000f404600000400550002000500260021207e004171000d010011000000000099898877f71001002e04804080402f0201010079000f400001100000011000000110000075005a400118')
+#c = generated.NGAP_PDU_Contents.InitialUEMessage
+#c.from_aper(b)
+
