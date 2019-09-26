c0f4128dfb80fc94c19ba9f38672f4a2a1402338
aligungr <aligng1620@gmail.com>
2019-09-26 09:07:56 -0300

    transpiler impl

--- main.py
+++ main.py
@@ -1,51 +1,85 @@
-from binascii import unhexlify, hexlify
-from pycrate import ngap
-
-InitiatingMessage = ngap.NGAP_PDU_Descriptions.InitiatingMessage
-
-print(InitiatingMessage.get_proto())
+import ngap
+from pycrate_asn1rt.dictobj import *
 
 
+def compile_element(element):
+    if type(element) is ASN1Dict:
+        return compile_object(element)
+    if type(element) is str:
+        return compile_primitive(element)
+    if type(element) is tuple:
+        if element[0] == "SEQUENCE":
+            return compile_object(element[1])
+        elif element[0] == "SEQUENCE OF":
+            return compile_array(element[1])
+        elif element[0] == "OPEN_TYPE":
+            return compile_open_type(element[1])
+        elif element[0] == "CHOICE":
+            return compile_choice(element[1])
+        else:
+            raise RuntimeError("not implemented yet")
+    raise RuntimeError("not implemented yet")
 
 
-exit(0)
+def compile_object(element):
+    if not type(element) is ASN1Dict:
+        raise RuntimeError("not implemented yet")
+    obj = {
+        "type": "object",
+        "properties": {}
+    }
+    for item in element._index:
+        if not type(item) is str:
+            raise RuntimeError("not implemented yet")
+        obj["properties"][item] = compile_element(element._dict[item])
+    return obj
 
 
-b = unhexlify("00000400550002000500260021207e004171000d010011000000000099898877f71001002e04804080402f0201010079000f400001100000011000000110000075005a400118")
-InitialUEMessage = ngap.NGAP_PDU_Contents.InitialUEMessage
-InitialUEMessage.from_aper(b)
-print(InitialUEMessage.to_json())
-
-exit(0)
-
-print(InitialUEMessage.get_proto())
-
-Value = {
-    "protocolIEs": [
-        {
-            "id": 85,
-            "criticality": "reject",
-            "value": ("RAN-UE-NGAP-ID", 5)
-        },
-        {
-            "id": 38,
-            "criticality": "reject",
-            "value": ("NAS-PDU", unhexlify("7e004171000d010011000000000099898877f71001002e04804080402f020101"))
-        },
-        #{
-        #    "id": 121,
-        #    "criticality": "reject",
-        #    "value": ("UserLocationInformation", unhexlify("400001100000011000000110000075"))
-        #}
-    ]
-}
-InitialUEMessage.set_val(Value)
-print(InitialUEMessage.to_asn1())
-print(hexlify(InitialUEMessage.to_aper()))
+def compile_array(element):
+    return {"type": "array", "items": compile_element(element)}
 
 
-#
-#b = unhexlify('000f404600000400550002000500260021207e004171000d010011000000000099898877f71001002e04804080402f0201010079000f400001100000011000000110000075005a400118')
-#c = generated.NGAP_PDU_Contents.InitialUEMessage
-#c.from_aper(b)
+def compile_primitive(element):
+    return element
 
+
+def compile_open_type(element):
+    if not type(element) is ASN1Dict:
+        raise RuntimeError("not implemented yet")
+    obj = {
+        "type": "open-type",
+        "properties": {}
+    }
+    for item in element._index:
+        if type(item) is str:
+            obj["properties"][item] = compile_element(element._dict[item])
+        elif type(item) is tuple and len(item) == 2:
+            if item[0] == "NGAP-IEs":  # todo: burda sıkıntı var gibi
+                obj["properties"][item[1]] = compile_element(element._dict[item])
+            else:
+                raise RuntimeError("not implemented yet")
+        else:
+            raise RuntimeError("not implemented yet")
+    return obj
+
+
+def compile_choice(element):
+    if not type(element) is ASN1Dict:
+        raise RuntimeError("not implemented yet")
+    obj = {
+        "type": "choice",
+        "properties": {}
+    }
+    for item in element._index:
+        if not type(item) is str:
+            raise RuntimeError("not implemented yet")
+        obj["properties"][item] = compile_element(element._dict[item])
+    return obj
+
+
+def compile_proto(element):
+    req_proto = element.get_proto(print_recurs=False)  # todo: loop ve ayrı defler
+    return compile_element(req_proto)
+
+
+print(compile_proto(ngap.NGAP_PDU_Contents.NGSetupRequest))
--- run.py
+++
@@ -1,4 +0,0 @@
-import sys
-sys.path.insert(1, 'pycrate')
-
-import main
