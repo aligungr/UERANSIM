ee99efa15f5ffc5a8fb4d4d143ad1f1f4537fee7
aligungr <aligng1620@gmail.com>
2019-09-27 16:09:02 -0300

    not eklendi

--- main.py
+++ main.py
@@ -13,5 +13,7 @@ print(json.dumps(compiled))
 ##### TODO: Bazı yerlerde type ve type_ref'in beraber bulunması uygun olmadı.
 #### Mesela jsonda 71. satırda RAN-UE-NGAP-ID var. bunu type_Ref'e eklediğimzide mesela,
 #### bunun integer olduğu ve min max değerlerinin ne olduğu zaten belli olması lazım
+#### --tabi bu durmunun bilgi kaybına yol açıp açmadığına mutalak bakıklsın. eğer bilgi kaubı yoksa,
+####   user defined olanlar için bu type yerne kullanılablir
 
 ### Bir de get type byb name kısmına modülü de dahil et hep. modül.type gibi olsun
