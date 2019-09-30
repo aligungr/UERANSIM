1059064d9c9b76a44250353ad694a9cd78938bb8
aligungr <aligng1620@gmail.com>
2019-09-30 08:30:42 -0300

    not kaldırıldı

--- main.py
+++ main.py
@@ -8,10 +8,3 @@ asn_type = asn2json.get_type_by_name(user_input[0], user_input[1])
 compiled = asn2json.compile_element(asn_type, None)
 
 print(json.dumps(compiled))
-
-
-##### TODO: Bazı yerlerde type ve type_ref'in beraber bulunması uygun olmadı.
-#### Mesela jsonda 71. satırda RAN-UE-NGAP-ID var. bunu type_Ref'e eklediğimzide mesela,
-#### bunun integer olduğu ve min max değerlerinin ne olduğu zaten belli olması lazım
-#### --tabi bu durmunun bilgi kaybına yol açıp açmadığına mutalak bakıklsın. eğer bilgi kaubı yoksa,
-####   user defined olanlar için bu type yerne kullanılablir
