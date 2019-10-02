631f38564ab7ca66c9225474fe5032b948685703
aligungr <aligng1620@gmail.com>
2019-10-02 15:26:39 -0300

    byte array impl.

---
+++ backendj/src/com/runsim/backend/utils/ByteArray.java
@@ -0,0 +1,13 @@
+package com.runsim.backend.utils;
+
+public class ByteArray {
+    private final byte[] data;
+
+    public ByteArray(byte[] data) {
+        this.data = data;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+}
