885fcab1dc633c517077770e428b05220c46e3b5
aligungr <aligng1620@gmail.com>
2019-10-04 14:56:51 -0300

    ip added

--- backend/src/main/java/com/runsim/backend/demo/AppClient.java
+++ backend/src/main/java/com/runsim/backend/demo/AppClient.java
@@ -8,9 +8,17 @@ import com.runsim.backend.otn.OtnString;
 public class AppClient {
 
     public static class TestMachine extends StateMachine {
+
         @Starter
         public Action starter(MachineContext machineContext) {
-            return Action.sendElement("NGAP_CommonDataTypes.Criticality", new OtnString("ignore"), "someState");
+
+
+            return Action.sendData(
+                    Utils.hexStringToByteArray("01231312321131204600000400550002000500260021207e004171000d010011000000000099898877f71001002e04804080402f0201010079000f400001100000011000000110000075005a400118"),
+                    "someState"
+            );
+
+            //return aaAction.sendElement("NGAP_CommonDataTypes.Criticality", new OtnString("ignore"), "someState");
         }
 
         @State
