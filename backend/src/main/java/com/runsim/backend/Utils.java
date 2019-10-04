44f83a8eb563facffaedbf2b1ee1d83730caab40
aligungr <aligng1620@gmail.com>
2019-10-04 10:59:05 -0300

    byteArrayToHexString added to utils

--- backend/src/main/java/com/runsim/backend/Utils.java
+++ backend/src/main/java/com/runsim/backend/Utils.java
@@ -24,4 +24,16 @@ public final class Utils {
         }
         return true;
     }
+
+    public static String byteArrayToHexString(byte[] bytes) {
+        final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
+
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
 }
