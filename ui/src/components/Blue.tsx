3c5f7d889c45d1c32d68efa44c0fef1f84f9edf6
ecakir <cakir-enes@outlook.com>
2019-10-07 10:53:49 +0300

    fix import style

--- ui/src/components/Blue.tsx
+++ ui/src/components/Blue.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
+import * as React from 'react'
 import { H1, Button } from '@blueprintjs/core'
 
 export const Hello = () => {
-  let [txt, setTxt] = useState('AA')
+  let [txt, setTxt] = React.useState('AA')
   return <Button text={txt} onClick={() => setTxt(Math.random().toFixed(3))} />
 }
