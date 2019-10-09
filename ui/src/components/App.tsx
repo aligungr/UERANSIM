793d6cd862e60b9717611eb4aea73b85b85461fd
aligungr <aligng1620@gmail.com>
2019-10-09 07:24:01 -0300

    socketClient introduced

--- ui/src/components/App.tsx
+++ ui/src/components/App.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react'
 import { Hello } from './Blue'
-import {SocketClient} from './SocketClient'
+import { SocketClient } from './SocketClient'
 
 export const App = () => {
-  let ws = new SocketClient();
+  let ws = new SocketClient()
 
   React.useEffect(() => {
-    document.body.className = 'bp3-dark';
+    document.body.className = 'bp3-dark'
   }, [])
   return <Hello />
 }
--- ui/src/components/SocketClient.tsx
+++ ui/src/components/SocketClient.tsx
@@ -1,35 +1,41 @@
-
-
 export class SocketClient {
   private ws: WebSocket
 
   constructor() {
-    const _this = this;
+    const _this = this
 
-    this.ws = new WebSocket("ws://localhost:5002");
-    this.ws.onmessage = function(e: MessageEvent) { _this.onMessage(e); }
-    this.ws.onopen = function(e: Event) { _this.onOpen(e); }
-    this.ws.onclose = function(e: CloseEvent) { _this.onClose(e); }
-    this.ws.onerror = function(e: Event) { _this.onError(e); }
+    this.ws = new WebSocket('ws://localhost:5002')
+    this.ws.onmessage = function(e: MessageEvent) {
+      _this.onMessage(e)
+    }
+    this.ws.onopen = function(e: Event) {
+      _this.onOpen(e)
+    }
+    this.ws.onclose = function(e: CloseEvent) {
+      _this.onClose(e)
+    }
+    this.ws.onerror = function(e: Event) {
+      _this.onError(e)
+    }
   }
 
   private onMessage(e: MessageEvent) {
-    console.log("onMessage, " + e);
+    console.log('onMessage, ' + e)
   }
 
   private onOpen(e: Event) {
-    console.log("onOpen, " + e);
+    console.log('onOpen, ' + e)
   }
 
   private onClose(e: CloseEvent) {
-    console.log("onClose, " + e);
+    console.log('onClose, ' + e)
   }
 
   private onError(e: Event) {
-    console.log("onError, " + e);
+    console.log('onError, ' + e)
   }
 
   send(data: string) {
-    this.ws.send(data);
+    this.ws.send(data)
   }
 }
