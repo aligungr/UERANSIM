3f0a9d15d8ac25cbc96f636946550c4224fafc0e
aligungr <aligng1620@gmail.com>
2019-10-09 05:18:28 -0300

    express and ws packages added

--- ui/src/components/App.tsx
+++ ui/src/components/App.tsx
@@ -1,6 +1,22 @@
 import * as React from 'react'
 import { Hello } from './Blue'
 
+import * as express from 'express';
+import * as http from 'http';
+import * as WebSocket from 'ws';
+
+//const app = express();
+//const server = http.createServer(app);
+//const wss = new WebSocket.Server({ server });
+//
+//wss.on('connection', (ws: WebSocket) => {
+//  ws.on('message', (message: string) => {
+//    console.log('received: %s', message);
+//    ws.send(`Hello, you sent -> ${message}`);
+//  });
+//  ws.send('Hi there, I am a WebSocket server');
+//});
+
 export const App = () => {
   React.useEffect(() => {
     document.body.className = 'bp3-dark'
