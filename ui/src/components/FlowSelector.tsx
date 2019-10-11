75fa1c7cd622a630e51fc37db798dd1b543dd5a7
aligungr <aligng1620@gmail.com>
2019-10-11 05:26:17 -0300

    flow selector impl

--- ui/src/components/FlowSelector.tsx
+++ ui/src/components/FlowSelector.tsx
@@ -1,14 +1,88 @@
 import * as React from 'react'
-import { Button, ButtonGroup, Collapse, Divider, Pre } from '@blueprintjs/core'
+import { Button, ButtonGroup, Collapse, Divider, MenuItem, Pre, ProgressBar, Spinner } from '@blueprintjs/core'
 import { App } from './App'
-import { Constants } from './Constants'
+import { Console } from './Console'
+import { ItemPredicate, ItemRenderer, Select } from '@blueprintjs/select'
+import { Main } from './Main'
 
-export class FlowSelector extends React.Component<any, any> {
+interface IFlow {
+  title: string;
+}
+
+const FlowSelect = Select.ofType<IFlow>()
+
+const itemRenderer: ItemRenderer<IFlow> = (flow, { handleClick, modifiers, query }) => {
+  if (!modifiers.matchesPredicate) {
+    return null
+  }
+  return (
+    <MenuItem
+      active={modifiers.active}
+      disabled={modifiers.disabled}
+      onClick={handleClick}
+      text={flow.title}
+    />
+  )
+}
+
+const itemFilter: ItemPredicate<IFlow> = (query, flow, _index, exactMatch) => {
+  const normalizedTitle = flow.title.toLowerCase().trim()
+  const normalizedQuery = query.toLowerCase().trim()
+  return normalizedTitle.includes(normalizedQuery)
+}
+
+interface IFlowSelectorState {
+  selected: IFlow | null
+  items: IFlow[]
+  loaded: boolean
+}
+
+export class FlowSelector extends React.Component<any, IFlowSelectorState> {
   constructor(props: any) {
     super(props)
+    this.state = { selected: null, items: [], loaded: false }
   }
 
-  render() {
-    return <div>sad</div>
+  public render() {
+    let content = (<Spinner/>)
+
+    if (this.state.loaded) {
+      content = (
+        <>
+          <FlowSelect
+            items={this.state.items}
+            itemPredicate={itemFilter}
+            noResults={<MenuItem disabled={true} text="No results."/>}
+            onItemSelect={(e: IFlow) => this.onItemSelect(e)}
+            itemRenderer={itemRenderer}>
+            <Button text={this.state.selected == null ? 'Select a Flow' : this.state.selected.title}
+                    icon="film"
+                    rightIcon="caret-down"/>
+          </FlowSelect>
+          <Button
+            style={{ marginLeft: '8px' }}
+            disabled={this.state.selected == null}
+            text={'Run Flow'} onClick={() => this.onRunClicked()}
+            intent={'primary'}/>
+        </>
+      )
+    }
+
+    return content
+  }
+
+  private onRunClicked() {
+    Console.log("run clicked");
+  }
+
+  private onItemSelect(item: IFlow) {
+    this.setState({ selected: item })
+  }
+
+  public startLoading() {
+    Main.sendMessage({
+      cmd: "get-all-flows",
+      args: {}
+    })
   }
 }
--- ui/src/components/Main.tsx
+++ ui/src/components/Main.tsx
@@ -2,37 +2,83 @@ import * as React from 'react'
 import { H1, Button, Classes, Card } from '@blueprintjs/core'
 import { Console } from './Console'
 import { FlowSelector } from './FlowSelector'
-import { SocketClient } from './SocketClient'
 import { Constants } from './Constants'
 import { Navigation } from './Navigation'
 
+export interface IMessage {
+  cmd: string,
+  args: object
+}
+
 export class Main extends React.Component<any, any> {
   public static instance: Main
-  private static ws: SocketClient
+  private ws: WebSocket | null = null
+
+  private flowSelector: FlowSelector | null = null
 
   constructor(props: any) {
     super(props)
     Main.instance = this
   }
 
-  render() {
+  public render() {
     return (
       <div style={{ margin: '8px' }}>
-        <Button
-          className={Classes.DARK}
-          text={'btn'}
-          onClick={() => {
-            if (console != null) {
-              Console.log('slm, ' + Math.random() + '')
-            }
-          }}
-        />
-        <FlowSelector />
+        <FlowSelector ref={r => this.flowSelector = r}/>
       </div>
     )
   }
 
-  componentDidMount(): void {
-    Main.ws = new SocketClient()
+  public componentDidMount() {
+    this.setupWebSocket()
+  }
+
+  private setupWebSocket() {
+    this.ws = new WebSocket('ws://localhost:5002')
+    const _this = this
+    this.ws.onmessage = function(e: MessageEvent) {
+      _this.onMessage(e)
+    }
+    this.ws.onopen = function(e: Event) {
+      _this.onOpen(e)
+    }
+    this.ws.onclose = function(e: CloseEvent) {
+      _this.onClose(e)
+    }
+    this.ws.onerror = function(e: Event) {
+      _this.onError(e)
+    }
+  }
+
+  private onMessage(e: MessageEvent) {
+    console.log(e.data);
+    Console.log(JSON.stringify(e), 'WebSocket')
+  }
+
+  private onOpen(e: Event) {
+    Console.success('connection established', 'WebSocket')
+    if (this.flowSelector != null) {
+      this.flowSelector.startLoading()
+    }
+  }
+
+  private onClose(e: CloseEvent) {
+    Console.success('connection closed', 'WebSocket')
+  }
+
+  private onError(e: Event) {
+    Console.error('error occured', 'WebSocket')
+  }
+
+  public static sendMessage(message: IMessage) {
+    Console.log(`sending message of type '${message.cmd}'.`, 'WebSocket')
+    const json = JSON.stringify(message)
+
+    if (Main.instance.ws == null){
+      Console.error("Main.instance.ws is null", 'WebSocket')
+      return
+    }
+
+    Main.instance.ws.send(json)
   }
 }
