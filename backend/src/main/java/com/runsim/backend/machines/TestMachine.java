58154dca95e81f456e62976be9f5ea2b7b417289
aligungr <aligng1620@gmail.com>
2019-10-15 11:35:00 +0300

    afd

--- backend/src/main/java/com/runsim/backend/machines/TestMachine.java
+++ backend/src/main/java/com/runsim/backend/machines/TestMachine.java
@@ -15,6 +15,9 @@ public class TestMachine {
 
     @Starter
     public Action starter(MachineContext machineContext) {
+
+        int age = 22;
+
         OtnElement obj = OtnXmlParser.parseXml(Utils.getResourceString("initialUEMessage.xml"));
         return Action.sendElement("NGAP_PDU_Contents.InitialUEMessage", obj, "someState");
         //return Action.sendData(Utils.hexStringToByteArray("000f403f0000040055000200010026001a197e004171000d010011000000000000000000f12e04808080800079000f400001100000011000000110000075005a400118"), "someState");
@@ -22,6 +25,8 @@ public class TestMachine {
 
     @State
     public Action someState(MessageContext messageContext, MachineContext machineContext) {
+        String name = "ali";
+
         return Action.closeConnection();
     }
 }
