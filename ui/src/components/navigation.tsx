8dab826614ccc66b9471f1e1300f1a79bf73154f
ecakir <tinkymortis@gmail.com>
2019-10-20 22:29:56 +0300

    'refactor'

--- ui/src/components/navigation.tsx
+++ ui/src/components/navigation.tsx
@@ -1,43 +1,33 @@
-import {
-  Alignment,
-  AnchorButton,
-  Navbar,
-  NavbarDivider,
-  NavbarGroup,
-  NavbarHeading,
-  Tooltip,
-} from '@blueprintjs/core'
-import * as React from 'react'
-import { useThemeStore, useAppStore, useConsoleStore } from '../basis/stores'
+import { Alignment, AnchorButton, Navbar, NavbarDivider, NavbarGroup, NavbarHeading, Tooltip } from '@blueprintjs/core';
+import * as React from 'react';
+import { useThemeStore, useAppStore, useConsoleStore } from '../basis/stores';
 
 export function Navigation(props: any) {
-  const themeStore = useThemeStore()
-  const appStore = useAppStore()
-  const consoleStore = useConsoleStore()
+	const themeStore = useThemeStore();
+	const appStore = useAppStore();
+	const consoleStore = useConsoleStore();
 
-  return (
-    <Navbar>
-      <NavbarGroup align={Alignment.LEFT}>
-        <NavbarHeading>{appStore.appName}</NavbarHeading>
-        <NavbarDivider />
-        <Tooltip content={`${consoleStore.isOpen ? 'Hide' : 'Show'} Console`}>
-          <AnchorButton
-            minimal={true}
-            rightIcon="console"
-            onClick={() => consoleStore.toggleOpen()}
-            active={consoleStore.isOpen}
-          />
-        </Tooltip>
-        <Tooltip
-          content={`Switch to ${themeStore.isDark ? 'Light' : 'Dark'} Theme`}
-        >
-          <AnchorButton
-            minimal={true}
-            rightIcon={themeStore.isDark ? 'flash' : 'moon'}
-            onClick={() => themeStore.toggleTheme()}
-          />
-        </Tooltip>
-      </NavbarGroup>
-    </Navbar>
-  )
+	return (
+		<Navbar>
+			<NavbarGroup align={Alignment.LEFT}>
+				<NavbarHeading>{appStore.appName}</NavbarHeading>
+				<NavbarDivider />
+				<Tooltip content={`${consoleStore.isOpen ? 'Hide' : 'Show'} Console`}>
+					<AnchorButton
+						minimal={true}
+						rightIcon="console"
+						onClick={() => consoleStore.toggleOpen()}
+						active={consoleStore.isOpen}
+					/>
+				</Tooltip>
+				<Tooltip content={`Switch to ${themeStore.isDark ? 'Light' : 'Dark'} Theme`}>
+					<AnchorButton
+						minimal={true}
+						rightIcon={themeStore.isDark ? 'flash' : 'moon'}
+						onClick={() => themeStore.toggleTheme()}
+					/>
+				</Tooltip>
+			</NavbarGroup>
+		</Navbar>
+	);
 }
