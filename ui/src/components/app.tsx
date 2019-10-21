d5e04bc37167feae593aa2e3ab436b162240aee1
aligungr <aligng1620@gmail.com>
2019-10-21 09:32:32 +0300

    refactoring

--- ui/src/components/app.tsx
+++ ui/src/components/app.tsx
@@ -2,16 +2,14 @@ import * as React from 'react';
 import { Navigation } from './navigation';
 import { MainContent } from './mainContent';
 import { LoggerV2 } from './loggerv2';
-import { useLogStore } from '../stores/log';
 
 export function App() {
-	const logStore = useLogStore();
 	return (
 		<div>
 			<Navigation />
 			<MainContent />
 			<LoggerV2 />
-			{/* <Logger /> */}
+			{/* <Logger /> */} /*todo: remove this*/
 		</div>
 	);
 }
