b014fa08c1ec5116752f4af2d9925c3febc0ab52
aligungr <aligng1620@gmail.com>
2019-10-22 13:00:45 +0300

    afd

--- ui/src/components/flowAction.tsx
+++ ui/src/components/flowAction.tsx
@@ -1,9 +1,25 @@
 import * as React from 'react'
+import { Button, Card, Elevation } from '@blueprintjs/core'
 
 export function FlowAction() {
   return (
     <div>
-      flow action weill  be here
+      <div style={{ width: '25%' }}>
+        <StatesSide/>
+      </div>
+      <div style={{ width: '75%' }}>
+        <StatesSide/>
+      </div>
     </div>
+  )
+}
+
+function StatesSide() {
+  return (
+    <Card interactive={false} elevation={Elevation.TWO}>
+      <h5><a href="#">Card heading</a></h5>
+      <p>Card content</p>
+      <Button>Submit</Button>
+    </Card>
   )
 }
