5504ad05aaa1c6213b901c0ac34e8f0beee92ac3
aligungr <aligng1620@gmail.com>
2019-10-24 10:44:59 +0300

    tests

--- mts/decoder.py
+++ mts/decoder.py
@@ -6,8 +6,11 @@ from pycrate_asn1rt.asnobj_ext import *
 from pycrate_asn1rt.asnobj_class import *
 import binascii
 
-hexs = "00000400550002000500260021207e004171000d010011000000000099898877f71001002e04804080402f0201010079000f400001100000011000000110000075005a400118"
-asn_type = ngap.NGAP_PDU_Contents.InitialUEMessage
+#hexs = "00000400550002000500260021207e004171000d010011000000000099898877f71001002e04804080402f0201010079000f400001100000011000000110000075005a400118"
+#asn_type = ngap.NGAP_PDU_Contents.InitialUEMessage
+
+hexs = "00044018000003000A0002000C00550002000100260005047E00446F"
+asn_type = ngap.NGAP_PDU_Descriptions.InitiatingMessage
 
 asn_type.from_aper(binascii.unhexlify(hexs))
 print(asn_type.to_json())
