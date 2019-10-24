d94daf6f2fa3c647c90bab1af03036b97bb963e3
aligungr <aligng1620@gmail.com>
2019-10-24 11:08:40 +0300

    afd

--- backend/src/main/java/com/runsim/backend/flows/TestFlow.java
+++ backend/src/main/java/com/runsim/backend/flows/TestFlow.java
@@ -2,16 +2,21 @@ package com.runsim.backend.flows;
 
 import com.runsim.backend.BaseFlow;
 import com.runsim.backend.Message;
+import com.runsim.backend.utils.Utils;
 
 public class TestFlow extends BaseFlow {
 
     public State main(Message message) {
-        sendData(3, 4, 5, 6);
-        return this::someInvalidDataSentAndWaitingAResponse;
+        sendBase16("000f403f0000040055000200010026001a197e004171000d010011000000000000000000f12e04808080800079000f400001100000011000000110000075005a400118");
+        return this::waitingRequests;
     }
 
-    private State someInvalidDataSentAndWaitingAResponse(Message message) {
-        System.err.println("message received " + message.getLength() + " bytes.");
+    private State waitingRequests(Message message) {
+        System.out.println("---------------");
+        System.out.println("message received " + message.getLength() + " bytes.");
+        System.out.println("---------------");
+        String mes = Utils.byteArrayToHexString(message.getRawData());
+
         return closeConnection();
     }
 }
