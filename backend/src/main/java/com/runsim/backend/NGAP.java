6dadf22c0b8f1747c74f0c092131dc04a7190587
aligungr <aligng1620@gmail.com>
2019-10-25 08:22:10 +0300

    refactoring

--- backend/src/main/java/com/runsim/backend/NGAP.java
+++ backend/src/main/java/com/runsim/backend/NGAP.java
@@ -5,19 +5,24 @@ import com.runsim.backend.ngap.ValueFactory;
 import com.runsim.backend.utils.Utils;
 import fr.marben.asnsdk.japi.Context;
 import fr.marben.asnsdk.japi.Loader;
+import fr.marben.asnsdk.japi.spe.Value;
 import fr.marben.asnsdk.japi.vi.IAbstractSyntax;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 
 public class NGAP {
+    private static final boolean TRACE = false;
     private static Context context;
 
     static {
         RuntimeConfiguration.initialize();
     }
 
-    public synchronized static Context getContext() {
+    private synchronized static Context getContext() {
         if (context != null)
             return context;
 
@@ -33,13 +38,41 @@ public class NGAP {
         context.setAbstractSyntax(asn);
         context.setValueFactory(new ValueFactory());
 
-        context.setTraceWriter(new PrintWriter(System.out, true));
-        context.setEncodingTraceWhileDecoding(true);
-        context.setEncodingTraceWhileEncoding(true);
-        context.setValueTraceWhileDecoding(true);
-        context.setValueTraceWhileEncoding(true);
-        context.setIndentedXerValueTrace(true);
+        if (TRACE) {
+            context.setTraceWriter(new PrintWriter(System.out, true));
+            context.setEncodingTraceWhileDecoding(true);
+            context.setEncodingTraceWhileEncoding(true);
+            context.setValueTraceWhileDecoding(true);
+            context.setValueTraceWhileEncoding(true);
+            context.setIndentedXerValueTrace(true);
+        }
+
+        context.setIndentationShift(2);
 
         return NGAP.context = context;
+    }
+
+    public static <T extends Value> T perDecode(Class<T> type, byte[] data) {
+        try {
+            var value = type.getConstructor().newInstance();
+            value.perDecode(getContext(), new ByteArrayInputStream(data));
+            return value;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static <T extends Value> T perDecode(Class<T> type, String base16) {
+        return perDecode(type, Utils.hexStringToByteArray(base16));
+    }
+
+    public static String xerEncode(Value value) {
+        try (var stream = new ByteArrayOutputStream()) {
+            value.xerEncode(getContext(), stream);
+            var data = stream.toByteArray();
+            return new String(data, StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- backend/src/main/java/com/runsim/backend/flows/RegistrationFlow.java
+++ backend/src/main/java/com/runsim/backend/flows/RegistrationFlow.java
@@ -6,14 +6,12 @@ import com.runsim.backend.NGAP;
 import com.runsim.backend.ngap.ngap_pdu_descriptions.NGAP_PDU;
 import com.runsim.backend.utils.Utils;
 
-import java.io.ByteArrayInputStream;
-
 public class RegistrationFlow extends BaseFlow {
 
     public State main(Message message) throws Exception {
-        new NGAP_PDU().perDecode(NGAP.getContext(), new ByteArrayInputStream(
-                Utils.hexStringToByteArray("000f403f0000040055000200010026001a197e004171000d010011000000000000000000f12e04808080800079000f400001100000011000000110000075005a400118")
-        ));
+        var pdu = NGAP.perDecode(NGAP_PDU.class, "000f403f0000040055000200010026001a197e004171000d010011000000000000000000f12e04808080800079000f400001100000011000000110000075005a400118");
+
+        var x = NGAP.xerEncode(pdu);
 
         //sendBase16("000f403f0000040055000200010026001a197e004171000d010011000000000000000000f12e04808080800079000f400001100000011000000110000075005a400118");
         return this::waitingRequests;
