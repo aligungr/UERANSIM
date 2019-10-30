b446d4e8b58896875cd4c3d0578bfecbdb97e08a
aligungr <aligng1620@gmail.com>
2019-10-30 10:18:38 +0300

    registration flow improvement

--- backend/src/main/java/com/runsim/backend/demo/FlowTesting.java
+++ backend/src/main/java/com/runsim/backend/demo/FlowTesting.java
@@ -1,18 +1,16 @@
 package com.runsim.backend.demo;
 
-import com.runsim.backend.nas.impl.ExtendedProtocolDiscriminator;
-import com.runsim.backend.nas.impl.PlainNASMessage;
-import com.runsim.backend.nas.impl.SecurityHeaderType;
+import com.runsim.backend.flows.RegistrationFlow;
 
 public class FlowTesting {
     public static void main(String[] args) throws Exception {
 
-        var plain = new PlainNASMessage();
+        /*var plain = new PlainNASMessage();
         plain.setExtendedProtocolDiscriminator(new ExtendedProtocolDiscriminator(ExtendedProtocolDiscriminator.MOBILITY_MANAGEMENT_MESSAGES));
         plain.setSecurityHeaderType(new SecurityHeaderType(SecurityHeaderType.INTEGRITY_PROTECTED_AND_CIPHERED));
-        System.err.println(plain);
+        System.err.println(plain);*/
 
-        //var flow = new RegistrationFlow();
-        //flow.start();
+        var flow = new RegistrationFlow();
+        flow.start();
     }
 }
--- backend/src/main/java/com/runsim/backend/flows/RegistrationFlow.java
+++ backend/src/main/java/com/runsim/backend/flows/RegistrationFlow.java
@@ -3,6 +3,8 @@ package com.runsim.backend.flows;
 import com.runsim.backend.BaseFlow;
 import com.runsim.backend.Message;
 import com.runsim.backend.NGAP;
+import com.runsim.backend.ngap.ngap_pdu_contents.DownlinkNASTransport;
+import com.runsim.backend.ngap.ngap_pdu_descriptions.InitiatingMessage;
 import com.runsim.backend.utils.Utils;
 
 public class RegistrationFlow extends BaseFlow {
@@ -19,6 +21,20 @@ public class RegistrationFlow extends BaseFlow {
         System.out.println("message received " + message.getLength() + " bytes.");
         System.out.println(NGAP.xerEncode(pdu));
         System.out.println("---------------");
+
+        if (!(pdu.getValue() instanceof InitiatingMessage)) {
+            System.err.println("bad message, InitiatingMessage is expected");
+            return this::waitingRequests;
+        }
+
+        var initiatingMessage = (InitiatingMessage) pdu.getValue();
+
+        if (!(initiatingMessage.value.getDecodedValue() instanceof DownlinkNASTransport)) {
+            System.err.println("bad message, DownlinkNASTransport is expected");
+            return this::waitingRequests;
+        }
+
+        var downlinkNASTransport = (DownlinkNASTransport) initiatingMessage.value.getDecodedValue();
 
         return closeConnection();
     }
