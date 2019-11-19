2ac4b70af720f71fff50874172077fb8f9ef6155
aligungr <aligng1620@gmail.com>
2019-11-19 08:31:31 +0300

    IENetworkName added

---
+++ backend/src/main/java/com/runsim/backend/nas/impl/ies/IENetworkName.java
@@ -0,0 +1,39 @@
+package com.runsim.backend.nas.impl.ies;
+
+import com.runsim.backend.nas.core.ies.InformationElement4;
+import com.runsim.backend.nas.impl.enums.EAddCountryInitials;
+import com.runsim.backend.nas.impl.enums.ECodingScheme;
+import com.runsim.backend.utils.OctetInputStream;
+import com.runsim.backend.utils.OctetOutputStream;
+import com.runsim.backend.utils.bits.Bit3;
+import com.runsim.backend.utils.octets.OctetN;
+import com.runsim.backend.utils.octets.OctetString;
+
+public class IENetworkName extends InformationElement4 {
+    public Bit3 numOfSpareBits;
+    public EAddCountryInitials addCi;
+    public ECodingScheme codingScheme;
+    public OctetString textString;
+
+    @Override
+    protected IENetworkName decodeIE4(OctetInputStream stream, int length) {
+        int flags = stream.readOctetI();
+
+        var res = new IENetworkName();
+        res.numOfSpareBits = new Bit3(flags);
+        res.addCi = EAddCountryInitials.fromValue(flags >> 3);
+        res.codingScheme = ECodingScheme.fromValue(flags >> 4);
+        res.textString = stream.readOctetString(length - 1);
+        return res;
+    }
+
+    @Override
+    public void encodeIE4(OctetOutputStream stream) {
+        var flags = new OctetN(0, 1);
+        flags = flags.setBitRange(0, 2, numOfSpareBits.intValue());
+        flags = flags.setBit(3, addCi.intValue());
+        flags = flags.setBitRange(4, 6, codingScheme.intValue());
+        stream.writeOctets(flags);
+        stream.writeOctetString(textString);
+    }
+}
