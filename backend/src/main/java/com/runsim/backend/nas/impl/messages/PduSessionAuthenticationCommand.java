7972ac7065942daa199c26cd271ed52660bf71a2
aligungr <aligng1620@gmail.com>
2019-11-22 09:37:37 +0300

    PduSessionAuthenticationCommand done

---
+++ backend/src/main/java/com/runsim/backend/nas/impl/messages/PduSessionAuthenticationCommand.java
@@ -0,0 +1,24 @@
+package com.runsim.backend.nas.impl.messages;
+
+import com.runsim.backend.nas.core.IMessageBuilder;
+import com.runsim.backend.nas.core.messages.PlainSmMessage;
+import com.runsim.backend.nas.impl.enums.EMessageType;
+import com.runsim.backend.nas.impl.ies.IEEapMessage;
+import com.runsim.backend.nas.impl.ies.IEExtendedProtocolConfigurationOptions;
+
+public class PduSessionAuthenticationCommand extends PlainSmMessage {
+    public IEEapMessage eapMessage;
+    public IEExtendedProtocolConfigurationOptions extendedProtocolConfigurationOptions;
+
+    public PduSessionAuthenticationCommand() {
+        super(EMessageType.PDU_SESSION_AUTHENTICATION_COMMAND);
+    }
+
+    @Override
+    public void build(IMessageBuilder builder) {
+        super.build(builder);
+
+        builder.mandatoryIE("eapMessage");
+        builder.optionalIE(0x7B, "extendedProtocolConfigurationOptions");
+    }
+}
