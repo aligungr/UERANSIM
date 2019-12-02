b068bf6c3643e3699826166422e445c1a51cbf56
aligungr <aligng1620@gmail.com>
2019-12-02 16:24:41 +0300

    mts impr.

--- backend/src/main/java/com/runsim/backend/demo/MtsTest.java
+++ backend/src/main/java/com/runsim/backend/demo/MtsTest.java
@@ -1,19 +1,14 @@
 package com.runsim.backend.demo;
 
 import com.runsim.backend.mts.MtsDecoder;
-import com.runsim.backend.mts.TypeRegistry;
 import com.runsim.backend.utils.Console;
 import com.runsim.backend.utils.Json;
 import com.runsim.backend.utils.Utils;
-import com.runsim.backend.utils.octets.Octet;
 
 public class MtsTest {
 
-    // todo: enumların duruöu
     public static void main(String[] args) throws Exception {
         var jsonString = Utils.getResourceString("mts.json");
-
-        TypeRegistry.register("Octet", Octet.class);
 
         var mtsDecoder = new MtsDecoder(false);
         var nasMessage = mtsDecoder.decode(jsonString);
--- backend/src/main/java/com/runsim/backend/mts/MtsConstruct.java
+++ backend/src/main/java/com/runsim/backend/mts/MtsConstruct.java
@@ -1,9 +1,11 @@
 package com.runsim.backend.mts;
 
 import com.runsim.backend.exceptions.MtsException;
+import com.runsim.backend.nas.core.ProtocolEnum;
 import com.runsim.backend.utils.Utils;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Map;
@@ -70,9 +72,62 @@ public class MtsConstruct {
     }
 
     public <T> T construct(Class<T> type, Map<String, Object> args) {
+        // TODO: buun gibi custom olanları cohernece yap.
+        if (ProtocolEnum.class.isAssignableFrom(type)) {
+            String identifier = null;
+            String name = null;
+            Integer value = null;
+
+            if (args.containsKey("identifier")) {
+                identifier = args.get("identifier").toString();
+            }
+            if (args.containsKey("name")) {
+                name = args.get("name").toString();
+            }
+            if (args.containsKey("value")) {
+                var obj = args.get("value");
+                if (!Traits.isNumber(obj.getClass()) /* && !Traits.isNumberIfString(obj) */)
+                    throw new MtsException("invalid value type for enum %s, it must be an int", type.getSimpleName());
+                value = new NumberInfo(obj.toString()).intValue();
+            }
+
+            if (identifier == null && name == null && value == null) {
+                throw new MtsException("specify identifier, name, or value for enum %s", type.getSimpleName());
+            }
+
+            if (identifier != null) {
+                if (name != null || value != null)
+                    throw new MtsException("specify exactly one of the following properties: identifier, name, or value for enum %s", type.getSimpleName());
+
+                var res = ProtocolEnum.fromIdentifier((Class<? extends ProtocolEnum>) type, identifier);
+                if (res == null)
+                    throw new MtsException("invalid value for enum %s", type.getSimpleName());
+                return (T) res;
+            }
+
+            if (value != null) {
+                if (name != null)
+                    throw new MtsException("specify exactly one of the following properties: identifier, name, or value for enum %s", type.getSimpleName());
+
+                var res = ProtocolEnum.fromIntValue((Class<? extends ProtocolEnum>) type, value);
+                if (res == null)
+                    throw new MtsException("invalid value for enum %s", type.getSimpleName());
+                return (T) res;
+            }
+
+            var res = ProtocolEnum.fromName((Class<? extends ProtocolEnum>) type, name);
+            if (res.size() == 0)
+                throw new MtsException("invalid value for enum %s", type.getSimpleName());
+            if (res.size() > 1)
+                throw new MtsException("multiple values matched for enum %s", type.getSimpleName());
+            return (T) res.get(0);
+        }
+
         var constructors = Arrays.asList(type.getDeclaredConstructors());
 
-        var matched = Utils.streamToList(constructors.stream().filter(constructor -> parameterCountMatches(constructor, args)));
+        var matched = Utils.streamToList(constructors.stream().filter(constructor -> Modifier.isPublic(constructor.getModifiers())));
+        matched = Utils.streamToList(matched.stream().filter(constructor -> parameterCountMatches(constructor, args)));
+
         if (matched.size() == 0) {
             throw new MtsException("no constructor found for %d parameters for type %s", args.size(), type.getSimpleName());
         }
@@ -103,9 +158,11 @@ public class MtsConstruct {
         for (int j = 0; j < params.length; j++) {
             var param = params[j];
             var value = args.get(param.getName());
+
             if (value instanceof ImplicitTypedValue) {
                 value = construct(param.getType(), ((ImplicitTypedValue) value).getParameters());
             }
+
             var conversions = mtsConvert.convert(value, param.getType());
 
             var shallowConversions = Utils.streamToList(conversions.stream().filter(conversion -> conversion.depth == 0));
