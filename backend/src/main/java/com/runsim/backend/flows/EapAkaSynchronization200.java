9f490e06d3b18552b25661f3cd5a246f72966756
farukemreciger <farukemreciger@gmail.com>
2019-12-05 13:07:17 +0300

    EapAkaSynchronization200 implentation started

---
+++ backend/src/main/java/com/runsim/backend/flows/EapAkaSynchronization200.java
@@ -0,0 +1,43 @@
+package com.runsim.backend.flows;
+
+import com.runsim.backend.BaseFlow;
+import com.runsim.backend.Message;
+import com.runsim.backend.nas.impl.enums.EMobileCountryCode;
+import com.runsim.backend.nas.impl.enums.EMobileNetworkCode3;
+import com.runsim.backend.nas.impl.ies.IE5gsRegistrationType;
+import com.runsim.backend.nas.impl.ies.IEImsiMobileIdentity;
+import com.runsim.backend.nas.impl.ies.IENasKeySetIdentifier;
+import com.runsim.backend.nas.impl.messages.AuthenticationResponse;
+import com.runsim.backend.nas.impl.messages.RegistrationRequest;
+import com.runsim.backend.nas.impl.values.VHomeNetworkPki;
+import com.runsim.backend.ngap.ngap_ies.RRCEstablishmentCause;
+import com.runsim.backend.ue.UeUtils;
+import com.runsim.backend.utils.bits.Bit3;
+
+public class EapAkaSynchronization200 extends BaseFlow {
+
+    public State main(Message message) {
+        return sendRegistrationRequest(message);
+    }
+
+    private State sendRegistrationRequest(Message message) {
+        var nasMessage = new RegistrationRequest();
+        nasMessage.mobileIdentity = new IEImsiMobileIdentity(EMobileCountryCode.fromValue(1), EMobileNetworkCode3.fromValue(1), "0000", IEImsiMobileIdentity.EProtectionSchemeIdentifier.NULL_SCHEMA, new VHomeNetworkPki(0), "000000028");
+        nasMessage.registrationType = new IE5gsRegistrationType(IE5gsRegistrationType.EFollowOnRequest.NO_FOR_PENDING, IE5gsRegistrationType.ERegistrationType.INITIAL_REGISTRATION);
+        nasMessage.nasKeySetIdentifier = new IENasKeySetIdentifier(IENasKeySetIdentifier.ETypeOfSecurityContext.NATIVE_SECURITY_CONTEXT, new Bit3(3));
+
+        var ngapPdu = UeUtils.createInitialUeMessage(nasMessage, 150, RRCEstablishmentCause.ASN_mo_Data);
+        sendPDU(ngapPdu);
+        return this::authenticationResponse;
+    }
+
+    private State authenticationResponse(Message message) {
+        var nasMessage = new AuthenticationResponse();
+        //nasMessage.eapMessage=new IEEapMessage()
+        var ngapPdu = UeUtils.createInitialUeMessage(nasMessage, 150, RRCEstablishmentCause.ASN_mo_Data);
+        //sendPDU(ngapPdu);
+
+        return closeConnection();
+    }
+
+}
