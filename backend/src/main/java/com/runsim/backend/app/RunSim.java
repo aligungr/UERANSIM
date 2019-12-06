43ab5bcb6a07d07e2cd14e62c9d2830f51914355
aligungr <aligng1620@gmail.com>
2019-12-06 15:58:05 +0300

    refact.

--- backend/src/main/java/com/runsim/backend/app/RunSim.java
+++ backend/src/main/java/com/runsim/backend/app/RunSim.java
@@ -4,11 +4,24 @@ import com.runsim.backend.Constants;
 import com.runsim.backend.app.sim.*;
 import com.runsim.backend.mts.MtsDecoder;
 import com.runsim.backend.mts.TypeRegistry;
+import com.runsim.backend.nas.NasEncoder;
+import com.runsim.backend.nas.core.messages.NasMessage;
 import com.runsim.backend.nas.eap.*;
+import com.runsim.backend.ngap.Values;
+import com.runsim.backend.ngap.ngap_commondatatypes.Criticality;
+import com.runsim.backend.ngap.ngap_commondatatypes.ProtocolIE_ID;
+import com.runsim.backend.ngap.ngap_ies.EmergencyFallbackRequestIndicator;
+import com.runsim.backend.ngap.ngap_ies.NAS_PDU;
+import com.runsim.backend.ngap.ngap_ies.RAN_UE_NGAP_ID;
+import com.runsim.backend.ngap.ngap_ies.RRCEstablishmentCause;
+import com.runsim.backend.ngap.ngap_pdu_contents.InitialUEMessage;
 import com.runsim.backend.utils.Console;
 import com.runsim.backend.utils.Utils;
+import fr.marben.asnsdk.japi.spe.OpenTypeValue;
 import io.github.classgraph.ClassGraph;
 import io.github.classgraph.ScanResult;
+
+import java.util.ArrayList;
 
 public class RunSim {
 
@@ -57,8 +70,45 @@ public class RunSim {
         MtsDecoder.setFileProvider(Utils::getResourceString);
     }
 
+    public static InitialUEMessage createInitialUeMessage(NasMessage nasMessage, int ranUeNgapIdValue, int establishmentCauseValue) {
+        var ranUeNgapId = new InitialUEMessage.ProtocolIEs.SEQUENCE();
+        ranUeNgapId.id = new ProtocolIE_ID(Values.NGAP_Constants__id_RAN_UE_NGAP_ID);
+        ranUeNgapId.criticality = new Criticality(Criticality.ASN_reject);
+        ranUeNgapId.value = new OpenTypeValue(new RAN_UE_NGAP_ID(ranUeNgapIdValue));
+
+        var nasPdu = new InitialUEMessage.ProtocolIEs.SEQUENCE();
+        nasPdu.id = new ProtocolIE_ID(Values.NGAP_Constants__id_NAS_PDU);
+        nasPdu.criticality = new Criticality(Criticality.ASN_reject);
+        nasPdu.value = new OpenTypeValue(new NAS_PDU(NasEncoder.nasPdu(nasMessage)));
+
+        var establishmentCause = new InitialUEMessage.ProtocolIEs.SEQUENCE();
+        establishmentCause.id = new ProtocolIE_ID(Values.NGAP_Constants__id_RRCEstablishmentCause);
+        establishmentCause.criticality = new Criticality(Criticality.ASN_ignore);
+        establishmentCause.value = new OpenTypeValue(new RRCEstablishmentCause(establishmentCauseValue));
+
+        var fake = new InitialUEMessage.ProtocolIEs.SEQUENCE();
+        fake.id = new ProtocolIE_ID(Values.NGAP_Constants__id_EmergencyFallbackIndicator);
+        fake.criticality = new Criticality(Criticality.ASN_ignore);
+        fake.value = new OpenTypeValue(new EmergencyFallbackRequestIndicator(establishmentCauseValue));
+
+        var protocolIEs = new ArrayList<InitialUEMessage.ProtocolIEs.SEQUENCE>();
+        protocolIEs.add(ranUeNgapId);
+        protocolIEs.add(nasPdu);
+        protocolIEs.add(establishmentCause);
+
+        var initialUEMessage = new InitialUEMessage();
+        initialUEMessage.protocolIEs = new InitialUEMessage.ProtocolIEs(protocolIEs);
+        return initialUEMessage;
+    }
+
     public static void main(String[] args) {
         initMts();
+
+/*
+        var x = NGAP.xerEncode(createInitialUeMessage(new RegistrationComplete(), 15, RRCEstablishmentCause.ASN_mo_Data));
+        Console.println(x);
+        if (1 == 1) return;
+        ;*/
 
         var nasMessage = MtsDecoder.decode("flow1.json");
         Console.println(Json.toJson(nasMessage));
--- backend/src/main/java/com/runsim/backend/app/sim/SimulationStep.java
+++ backend/src/main/java/com/runsim/backend/app/sim/SimulationStep.java
@@ -1,11 +1,16 @@
 package com.runsim.backend.app.sim;
 
+import com.google.gson.JsonElement;
 import com.runsim.backend.nas.core.messages.NasMessage;
 
 public class SimulationStep {
+    public final String ngapType;
+    public final Object[] ngapIEs;
     public final NasMessage nasMessage;
 
-    public SimulationStep(NasMessage nasMessage) {
+    public SimulationStep(String ngapType, JsonElement[] ngapIEs, NasMessage nasMessage) {
+        this.ngapType = ngapType;
+        this.ngapIEs = ngapIEs;
         this.nasMessage = nasMessage;
     }
 }
