5a302dfc3d639e964ba707c7c852818c1e81229d
aligungr <aligng1620@gmail.com>
2019-12-23 15:45:24 +0300

    AssociationHandler uncomment

--- ueransim/src/main/java/tr/havelsan/ueransim/sctp/AssociationHandler.java
+++ ueransim/src/main/java/tr/havelsan/ueransim/sctp/AssociationHandler.java
@@ -11,20 +11,20 @@ public class AssociationHandler extends AbstractNotificationHandler<PrintStream>
 
     @Override
     public HandlerResult handleNotification(AssociationChangeNotification notification, PrintStream attachment) {
-        /*if (notification.event() == AssociationChangeNotification.AssocChangeEvent.COMM_UP) {
+        if (notification.event() == AssociationChangeNotification.AssocChangeEvent.COMM_UP) {
             int outbound = notification.association().maxOutboundStreams();
             int inbound = notification.association().maxInboundStreams();
             attachment.printf(
                     "New association setup with %d outbound streams" + ", and %d inbound streams.\n",
                     outbound, inbound
             );
-        }*/
+        }
         return HandlerResult.CONTINUE;
     }
 
     @Override
     public HandlerResult handleNotification(ShutdownNotification notification, PrintStream attachment) {
-        //attachment.print("The association has been shutdown.\n");
+        attachment.print("The association has been shutdown.\n");
         return HandlerResult.RETURN;
     }
 }
