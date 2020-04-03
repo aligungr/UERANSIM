9b1dea35c5aaeb6b098680330eb3089697bea483
aligungr <aligng1620@gmail.com>
2020-04-03 15:28:06 +0300

    crypto module created

---
+++ crypto/src/main/java/tr/havelsan/ueransim/crypto/EIA2_128.java
@@ -0,0 +1,11 @@
+package tr.havelsan.ueransim.crypto;
+
+import tr.havelsan.ueransim.utils.bits.BitString;
+
+public class EIA2_128 {
+
+    public static boolean integrityCheck(BitString message) {
+        // todo
+        return true;
+    }
+}
