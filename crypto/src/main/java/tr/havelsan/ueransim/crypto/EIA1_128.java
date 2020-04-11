82b005f09e024930301323b59bb1f270ecab553e
aligungr <aligng1620@gmail.com>
2020-04-11 23:37:49 +0300

    128-EIA1 implemented

---
+++ crypto/src/main/java/tr/havelsan/ueransim/crypto/EIA1_128.java
@@ -0,0 +1,22 @@
+package tr.havelsan.ueransim.crypto;
+
+import tr.havelsan.ueransim.utils.bits.Bit;
+import tr.havelsan.ueransim.utils.bits.Bit5;
+import tr.havelsan.ueransim.utils.bits.BitString;
+import tr.havelsan.ueransim.utils.octets.Octet4;
+import tr.havelsan.ueransim.utils.octets.OctetString;
+
+public class EIA1_128 {
+
+    public static Octet4 computeMac(Octet4 count, Bit5 bearer, Bit direction, BitString message, OctetString key) {
+        var freshBits = new BitString();
+        for (int i = 0; i < 5; i++) {
+            freshBits.set(i, bearer.getBitB(i));
+        }
+        freshBits.clear(31);
+
+        var octets = freshBits.toOctetArray();
+        var fresh = new Octet4(octets[0], octets[1], octets[2], octets[3]);
+        return UIA2.computeMac(count, fresh, direction, message, key);
+    }
+}
