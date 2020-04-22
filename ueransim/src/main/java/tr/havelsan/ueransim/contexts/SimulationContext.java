2641e168986a652be75cc7c43745fc9485a980f6
aligungr <aligng1620@gmail.com>
2020-04-22 20:57:05 +0300

    NasSecurityContext added to SimulationContext

--- ueransim/src/main/java/tr/havelsan/ueransim/contexts/SimulationContext.java
+++ ueransim/src/main/java/tr/havelsan/ueransim/contexts/SimulationContext.java
@@ -1,15 +1,22 @@
 package tr.havelsan.ueransim.contexts;
 
+import tr.havelsan.ueransim.nas.NasSecurityContext;
 import tr.havelsan.ueransim.sctp.SCTPClient;
 
 public class SimulationContext {
     private final SCTPClient sctpClient;
+    private NasSecurityContext nasSecurityContext;
 
     public SimulationContext(SCTPClient sctpClient) {
         this.sctpClient = sctpClient;
+        this.nasSecurityContext = null;
     }
 
     public SCTPClient getSctpClient() {
         return sctpClient;
+    }
+
+    public NasSecurityContext getNasSecurityContext() {
+        return nasSecurityContext;
     }
 }
