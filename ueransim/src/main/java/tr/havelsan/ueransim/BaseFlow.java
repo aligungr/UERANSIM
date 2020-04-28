bb8652a379186f298c9259c3a4ee1ac7bd047913
aligungr <aligng1620@gmail.com>
2020-04-28 21:14:20 +0300

    AMF-UE-NGAP-ID moved to SimulationContext

--- ueransim/src/main/java/tr/havelsan/ueransim/BaseFlow.java
+++ ueransim/src/main/java/tr/havelsan/ueransim/BaseFlow.java
@@ -87,6 +87,10 @@ public abstract class BaseFlow {
         }
 
         // Adding AMF-UE-NGAP-ID (if any)
+        // TODO:
+        // This protocol ie should be included in all UE associated signalling.
+        // AMF-UE-NGAP-ID may be ignored for non UE associated messages.
+        // But currently this ie is added to all messages (if there is an AMF-UE-NGAP-ID in the context).
         long amfUeNgapId = simContext.getAmfUeNgapId();
         if (amfUeNgapId != 0) {
             // NOTE: criticality is hardcoded here, it may be changed
