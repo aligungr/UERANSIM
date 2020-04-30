90a5bd7472a043f5fe002c11fd3038d5c34bc879
aligungr <aligng1620@gmail.com>
2020-04-30 19:32:39 +0300

    RegistrationFlow refactored

--- ueransim/src/main/java/tr/havelsan/ueransim/flows/RegistrationFlow.java
+++ ueransim/src/main/java/tr/havelsan/ueransim/flows/RegistrationFlow.java
@@ -1,14 +1,9 @@
 package tr.havelsan.ueransim.flows;
 
-import threegpp.milenage.Milenage;
-import threegpp.milenage.MilenageBufferFactory;
-import threegpp.milenage.MilenageResult;
-import threegpp.milenage.biginteger.BigIntegerBuffer;
-import threegpp.milenage.biginteger.BigIntegerBufferFactory;
-import threegpp.milenage.cipher.Ciphers;
 import tr.havelsan.ueransim.BaseFlow;
 import tr.havelsan.ueransim.IncomingMessage;
 import tr.havelsan.ueransim.contexts.SimulationContext;
+import tr.havelsan.ueransim.crypto.Milenage;
 import tr.havelsan.ueransim.flowinputs.RegistrationInput;
 import tr.havelsan.ueransim.nas.core.messages.NasMessage;
 import tr.havelsan.ueransim.nas.eap.Eap;
@@ -24,24 +19,17 @@ import tr.havelsan.ueransim.ngap2.NgapCriticality;
 import tr.havelsan.ueransim.ngap2.NgapProcedure;
 import tr.havelsan.ueransim.utils.Color;
 import tr.havelsan.ueransim.utils.Console;
-import tr.havelsan.ueransim.utils.Utils;
-import tr.havelsan.ueransim.utils.octets.Octet;
 import tr.havelsan.ueransim.utils.octets.OctetString;
 
 import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class RegistrationFlow extends BaseFlow {
 
     private final RegistrationInput input;
-    private final MilenageBufferFactory<BigIntegerBuffer> milenageBufferFactory;
 
     public RegistrationFlow(SimulationContext simContext, RegistrationInput input) {
         super(simContext);
         this.input = input;
-        this.milenageBufferFactory = BigIntegerBufferFactory.getInstance();
     }
 
     @Override
@@ -115,72 +103,31 @@ public class RegistrationFlow extends BaseFlow {
     }
 
     private State handleAuthenticationRequest(AuthenticationRequest message) {
-        OctetString mac;
-        byte[] res;
-        Octet id;
-
         // Handle request
-        {
-            var akaPrime = (EapAkaPrime) message.eapMessage.eap;
-            var rand = akaPrime.attributes.get(EapAkaPrime.EAttributeType.AT_RAND);
-            rand = rand.substring(2); // reserved octets
+        var eapRequest = (EapAkaPrime) message.eapMessage.eap;
+        var rand = eapRequest.attributes.get(EapAkaPrime.EAttributeType.AT_RAND).substring(2).toHexString(false);
+        var id = eapRequest.id;
 
-            mac = akaPrime.attributes.get(EapAkaPrime.EAttributeType.AT_MAC);
-            id = akaPrime.id;
-
-            final String OP = input.eapAkaInput.OP;
-            final String SQN = input.eapAkaInput.SQN;
-            final String AMF = input.eapAkaInput.AMF;
-            final String KEY = input.eapAkaInput.KEY;
-
-            byte[] op = Utils.hexStringToByteArray(OP);
-            byte[] sqn = Utils.hexStringToByteArray(SQN);
-            byte[] amf = Utils.hexStringToByteArray(AMF);
-
-            var cipher = Ciphers.createRijndaelCipher(Utils.hexStringToByteArray(KEY));
-
-            byte[] opc = Milenage.calculateOPc(op, cipher, milenageBufferFactory);
-
-            Milenage<BigIntegerBuffer> milenage = new Milenage<>(opc, cipher, milenageBufferFactory);
-            ExecutorService executorService = Executors.newCachedThreadPool();
-
-            Map<MilenageResult, byte[]> result;
-            try {
-                result = milenage.calculateAll(rand.substring(0).toByteArray(), sqn, amf, executorService);
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-
-            executorService.shutdown();
-            res = result.get(MilenageResult.RES);
-
-            int padding = (res.length) % 4;
-            byte[] paddedRes = new byte[res.length + padding + 2];
-            System.arraycopy(res, 0, paddedRes, padding + 2, res.length);
-            int resBitLength = (paddedRes.length - 2) * 8;
-            paddedRes[0] = (byte) ((resBitLength >> 8) & 0xFF);
-            paddedRes[1] = (byte) (resBitLength & 0xFF);
-            res = paddedRes;
-        }
+        OctetString mac = eapRequest.attributes.get(EapAkaPrime.EAttributeType.AT_MAC);
+        byte[] res = Milenage.computeRes(input.eapAkaInput.KEY, input.eapAkaInput.OP, rand,
+                input.eapAkaInput.SQN, input.eapAkaInput.AMF, true);
 
         // Send response
-        {
-            var response = new AuthenticationResponse();
-            response.authenticationResponseParameter =
-                    new IEAuthenticationResponseParameter(input.authenticationResponseParameter);
+        var eapResponse = new EapAkaPrime(Eap.ECode.RESPONSE, id);
+        eapResponse.subType = EapAkaPrime.ESubType.AKA_CHALLENGE;
+        eapResponse.attributes = new LinkedHashMap<>();
+        eapResponse.attributes.put(EapAkaPrime.EAttributeType.AT_RES, new OctetString(res));
+        eapResponse.attributes.put(EapAkaPrime.EAttributeType.AT_MAC, mac);
+        eapResponse.attributes.put(EapAkaPrime.EAttributeType.AT_KDF, new OctetString("0001"));
 
-            var akaPrime = new EapAkaPrime(Eap.ECode.RESPONSE, id);
-            akaPrime.subType = EapAkaPrime.ESubType.AKA_CHALLENGE;
-            akaPrime.attributes = new LinkedHashMap<>();
-            akaPrime.attributes.put(EapAkaPrime.EAttributeType.AT_RES, new OctetString(res));
-            akaPrime.attributes.put(EapAkaPrime.EAttributeType.AT_MAC, mac);
-            akaPrime.attributes.put(EapAkaPrime.EAttributeType.AT_KDF, new OctetString("0001"));
-            response.eapMessage = new IEEapMessage(akaPrime);
+        var response = new AuthenticationResponse();
+        response.authenticationResponseParameter =
+                new IEAuthenticationResponseParameter(input.authenticationResponseParameter);
+        response.eapMessage = new IEEapMessage(eapResponse);
 
-            send(new NgapBuilder(NgapProcedure.UplinkNASTransport, NgapCriticality.IGNORE)
-                    .addRanUeNgapId(input.ranUeNgapId, NgapCriticality.REJECT)
-                    .addUserLocationInformationNR(input.userLocationInformationNr, NgapCriticality.IGNORE), response);
-        }
+        send(new NgapBuilder(NgapProcedure.UplinkNASTransport, NgapCriticality.IGNORE)
+                .addRanUeNgapId(input.ranUeNgapId, NgapCriticality.REJECT)
+                .addUserLocationInformationNR(input.userLocationInformationNr, NgapCriticality.IGNORE), response);
 
         return this::waitAmfMessages;
     }
