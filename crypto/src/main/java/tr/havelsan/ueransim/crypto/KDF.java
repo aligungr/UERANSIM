34a77b80dd4c9c079199c3653a994b006d08afab
aligungr <aligng1620@gmail.com>
2020-05-05 18:10:54 +0300

    encodeString added to KDF

--- crypto/src/main/java/tr/havelsan/ueransim/crypto/KDF.java
+++ crypto/src/main/java/tr/havelsan/ueransim/crypto/KDF.java
@@ -6,17 +6,35 @@ import tr.havelsan.ueransim.utils.octets.OctetString;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.text.Normalizer;
 
 public class KDF {
 
+    /**
+     * Calculates derived key based on given parameters as specified in 3GPP TS 33.220
+     */
     public static OctetString calculateKey(OctetString key, Octet fc, OctetString[] parameters) {
-        var s = calculateS(new OctetString(new Octet[]{fc}), parameters);
-        return hmacSha256(key, s);
+        return hmacSha256(key, calculateS(new OctetString(new Octet[]{fc}), parameters));
     }
 
+    /**
+     * Calculates derived key based on given parameters as specified in 3GPP TS 33.220
+     */
     public static OctetString calculateKey(OctetString key, Octet fc1, Octet fc2, OctetString[] parameters) {
-        var s = calculateS(new OctetString(new Octet[]{fc1, fc2}), parameters);
-        return hmacSha256(key, s);
+        return hmacSha256(key, calculateS(new OctetString(new Octet[]{fc1, fc2}), parameters));
+    }
+
+    /**
+     * Encodes given character string to octet string as specified in 3GPP TS 33.220
+     */
+    public static OctetString encodeString(String string) {
+        // V16.0.0 - B.2.1.2 Character string encoding
+        // A character string shall be encoded to an octet string according to UTF-8 encoding rules as specified in
+        // IETF RFC 3629 [24] and apply Normalization Form KC (NFKC) as specified in [37].
+        String normalized = Normalizer.normalize(string, Normalizer.Form.NFKC);
+        byte[] bytes = normalized.getBytes(StandardCharsets.UTF_8);
+        return new OctetString(bytes);
     }
 
     private static OctetString calculateS(OctetString fc, OctetString[] parameters) {
