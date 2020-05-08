a4fc63563e39fead21039ab4235493c5a7188210
aligungr <aligng1620@gmail.com>
2020-05-08 18:07:41 +0300

    calculateCk and calculateIk added to Milenage

--- crypto/src/main/java/tr/havelsan/ueransim/crypto/Milenage.java
+++ crypto/src/main/java/tr/havelsan/ueransim/crypto/Milenage.java
@@ -25,4 +25,32 @@ public class Milenage {
         byte[] res = result.get(MilenageResult.RES);
         return new OctetString(res);
     }
+
+    /**
+     * Calculates CK with given parameters.
+     */
+    public static OctetString calculateCk(OctetString key, OctetString op, OctetString rand) {
+        var factory = BigIntegerBufferFactory.getInstance();
+        var cipher = Ciphers.createRijndaelCipher(key.toByteArray());
+
+        byte[] opc = threegpp.milenage.Milenage.calculateOPc(op.toByteArray(), cipher, factory);
+
+        var milenage = new threegpp.milenage.Milenage<>(opc, cipher, factory);
+
+        return new OctetString(milenage.f3(rand.toByteArray()));
+    }
+
+    /**
+     * Calculates IK with given parameters.
+     */
+    public static OctetString calculateIk(OctetString key, OctetString op, OctetString rand) {
+        var factory = BigIntegerBufferFactory.getInstance();
+        var cipher = Ciphers.createRijndaelCipher(key.toByteArray());
+
+        byte[] opc = threegpp.milenage.Milenage.calculateOPc(op.toByteArray(), cipher, factory);
+
+        var milenage = new threegpp.milenage.Milenage<>(opc, cipher, factory);
+
+        return new OctetString(milenage.f4(rand.toByteArray()));
+    }
 }
