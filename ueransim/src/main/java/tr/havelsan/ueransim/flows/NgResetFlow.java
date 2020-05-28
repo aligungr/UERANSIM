482829af0771288af1c05459df6971d919ff253c
aligungr <aligng1620@gmail.com>
2020-05-28 17:14:22 +0300

    NgResetFlow refactor

--- ueransim/src/main/java/tr/havelsan/ueransim/flows/NgResetFlow.java
+++ ueransim/src/main/java/tr/havelsan/ueransim/flows/NgResetFlow.java
@@ -2,20 +2,14 @@ package tr.havelsan.ueransim.flows;
 
 import tr.havelsan.ueransim.*;
 import tr.havelsan.ueransim.core.SimulationContext;
-import tr.havelsan.ueransim.ngap.ngap_ies.Cause;
-import tr.havelsan.ueransim.ngap.ngap_ies.CauseRadioNetwork;
 import tr.havelsan.ueransim.ngap.ngap_ies.ResetAll;
 import tr.havelsan.ueransim.ngap.ngap_ies.ResetType;
-import tr.havelsan.ueransim.ngap.ngap_pdu_contents.NGReset;
 import tr.havelsan.ueransim.ngap.ngap_pdu_contents.NGResetAcknowledge;
 import tr.havelsan.ueransim.ngap2.NgapBuilder;
 import tr.havelsan.ueransim.ngap2.NgapCause;
 import tr.havelsan.ueransim.ngap2.NgapCriticality;
 import tr.havelsan.ueransim.ngap2.NgapProcedure;
 
-import java.util.ArrayList;
-
-import static tr.havelsan.ueransim.ngap.ngap_ies.Cause.ASN_radioNetwork;
 import static tr.havelsan.ueransim.ngap.ngap_ies.ResetType.ASN_nG_Interface;
 
 public class NgResetFlow extends BaseFlow {
@@ -26,23 +20,15 @@ public class NgResetFlow extends BaseFlow {
 
     @Override
     public State main(IncomingMessage message) throws Exception {
-
-        var causeRadioNetwork = new CauseRadioNetwork(CauseRadioNetwork.ASN_user_inactivity);
-        var cause = new Cause(ASN_radioNetwork,causeRadioNetwork);
         var ngapCause = NgapCause.RADIO_NETWORK_USER_INACTIVITY;
 
-        var ngReset = new NGReset();
-        ngReset.protocolIEs = new NGReset.ProtocolIEs();
-        ngReset.protocolIEs.valueList = new ArrayList();
-        ngReset.protocolIEs.valueList.add(cause);
-
         var resetAll = new ResetAll();
-        var resetType = new ResetType(ASN_nG_Interface,resetAll);
-
-        ngReset.protocolIEs.valueList.add(resetType);
+        var resetType = new ResetType(ASN_nG_Interface, resetAll);
 
         send(new SendingMessage(
-                new NgapBuilder(NgapProcedure.NGReset, NgapCriticality.REJECT).addCause(ngapCause, NgapCriticality.IGNORE).addProtocolIE(resetType,NgapCriticality.REJECT), null
+                new NgapBuilder(NgapProcedure.NGReset, NgapCriticality.REJECT)
+                        .addCause(ngapCause, NgapCriticality.IGNORE)
+                        .addProtocolIE(resetType, NgapCriticality.REJECT), null
         ));
 
         return this::waitNgResetAcknowledge;
