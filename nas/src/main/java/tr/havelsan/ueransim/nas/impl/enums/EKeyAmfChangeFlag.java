708924e4b0303fd3dbc1062234bc3b74ed110af2
aligungr <aligng1620 @gmail.com>
2020-06-23 13:17:16 +0000

    IEIntraN1ModeNasTransparentContainer implemented

---
+++ nas/src/main/java/tr/havelsan/ueransim/nas/impl/enums/EKeyAmfChangeFlag.java
@@ -0,0 +1,16 @@
+package tr.havelsan.ueransim.nas.impl.enums;
+
+import tr.havelsan.ueransim.nas.core.ProtocolEnum;
+
+public class EKeyAmfChangeFlag extends ProtocolEnum {
+    public static final EKeyAmfChangeFlag CALCULATED = new EKeyAmfChangeFlag(0b0, "a new K_AMF has not been calculated by the network");
+    public static final EKeyAmfChangeFlag NOT_CALCULATED = new EKeyAmfChangeFlag(0b1, "a new K_AMF has been calculated by the network");
+
+    private EKeyAmfChangeFlag(int value, String name) {
+        super(value, name);
+    }
+
+    public static EKeyAmfChangeFlag fromValue(int value) {
+        return fromValueGeneric(EKeyAmfChangeFlag.class, value, null);
+    }
+}
