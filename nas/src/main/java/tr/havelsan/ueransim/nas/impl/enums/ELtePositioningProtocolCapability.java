df175014c980ac820447c0daf77c96d61cd929b9
aligungr <aligng1620 @gmail.com>
2020-06-23 14:17:17 +0000

    IE5gMmCapability bug fix and refactor

---
+++ nas/src/main/java/tr/havelsan/ueransim/nas/impl/enums/ELtePositioningProtocolCapability.java
@@ -0,0 +1,44 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 ALİ GÜNGÖR
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * @author Ali Güngör (aligng1620@gmail.com)
+ */
+
+package tr.havelsan.ueransim.nas.impl.enums;
+
+import tr.havelsan.ueransim.nas.core.ProtocolEnum;
+
+public class ELtePositioningProtocolCapability extends ProtocolEnum {
+    public static final ELtePositioningProtocolCapability NOT_SUPPORTED
+            = new ELtePositioningProtocolCapability(0b0, "LPP in N1 mode not supported");
+    public static final ELtePositioningProtocolCapability SUPPORTED
+            = new ELtePositioningProtocolCapability(0b1, "LPP in N1 mode supported");
+
+    private ELtePositioningProtocolCapability(int value, String name) {
+        super(value, name);
+    }
+
+    public static ELtePositioningProtocolCapability fromValue(int value) {
+        return fromValueGeneric(ELtePositioningProtocolCapability.class, value, null);
+    }
+}
