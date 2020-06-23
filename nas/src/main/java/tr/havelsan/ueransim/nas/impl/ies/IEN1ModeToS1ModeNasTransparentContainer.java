22d0f04e3393b2f88cef23d755d233f42e4ef39a
aligungr <aligng1620 @gmail.com>
2020-06-23 13:20:27 +0000

    IEN1ModeToS1ModeNasTransparentContainer implemented

---
+++ nas/src/main/java/tr/havelsan/ueransim/nas/impl/ies/IEN1ModeToS1ModeNasTransparentContainer.java
@@ -0,0 +1,27 @@
+package tr.havelsan.ueransim.nas.impl.ies;
+
+import tr.havelsan.ueransim.nas.core.ies.InformationElement3;
+import tr.havelsan.ueransim.utils.OctetInputStream;
+import tr.havelsan.ueransim.utils.OctetOutputStream;
+import tr.havelsan.ueransim.utils.octets.Octet;
+
+public class IEN1ModeToS1ModeNasTransparentContainer extends InformationElement3 {
+    public Octet sequenceNumber;
+
+    public IEN1ModeToS1ModeNasTransparentContainer() {
+    }
+
+    public IEN1ModeToS1ModeNasTransparentContainer(Octet sequenceNumber) {
+        this.sequenceNumber = sequenceNumber;
+    }
+
+    @Override
+    protected InformationElement3 decodeIE3(OctetInputStream stream) {
+        return new IEN1ModeToS1ModeNasTransparentContainer(stream.readOctet());
+    }
+
+    @Override
+    public void encodeIE3(OctetOutputStream stream) {
+        stream.writeOctet(sequenceNumber);
+    }
+}
