b7cf79326184023e8a81f67c1b6af8c8e7abe51e
aligungr <aligng1620@gmail.com>
2020-06-30 14:42:13 +0300

    refactored

--- ueransim/src/main/java/tr/havelsan/ueransim/api/gnb/GnbInterfaceManagement.java
+++ ueransim/src/main/java/tr/havelsan/ueransim/api/gnb/GnbInterfaceManagement.java
@@ -28,6 +28,7 @@ package tr.havelsan.ueransim.api.gnb;
 
 import tr.havelsan.ueransim.Ngap;
 import tr.havelsan.ueransim.core.GnbSimContext;
+import tr.havelsan.ueransim.ngap.Values;
 import tr.havelsan.ueransim.ngap.ngap_ies.PagingDRX;
 import tr.havelsan.ueransim.ngap.ngap_pdu_contents.NGSetupFailure;
 import tr.havelsan.ueransim.ngap.ngap_pdu_contents.NGSetupResponse;
@@ -36,8 +37,6 @@ import tr.havelsan.ueransim.ngap2.NgapCriticality;
 import tr.havelsan.ueransim.ngap2.NgapProcedure;
 import tr.havelsan.ueransim.utils.Logging;
 import tr.havelsan.ueransim.utils.Tag;
-
-import static tr.havelsan.ueransim.ngap.Values.NGAP_Constants__id_DefaultPagingDRX;
 
 public class GnbInterfaceManagement {
 
@@ -49,7 +48,7 @@ public class GnbInterfaceManagement {
                 new NgapBuilder(NgapProcedure.NGSetupRequest, NgapCriticality.REJECT)
                         .addProtocolIE(Ngap.createGlobalGnbId(ctx.config.gnbId, ctx.config.gnbPlmn), NgapCriticality.REJECT)
                         .addProtocolIE(Ngap.createSupportedTAList(ctx.config.supportedTAs), NgapCriticality.REJECT)
-                        .addProtocolIE(new PagingDRX(PagingDRX.ASN_v64), NgapCriticality.IGNORE, NGAP_Constants__id_DefaultPagingDRX)
+                        .addProtocolIE(new PagingDRX(PagingDRX.ASN_v64), NgapCriticality.IGNORE, Values.NGAP_Constants__id_DefaultPagingDRX)
         );
 
         Logging.funcOut();
