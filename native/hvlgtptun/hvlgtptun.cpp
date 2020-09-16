af12716820f1332471065ff5de5a1d0dd497fd87
aligungr <aligng1620@gmail.com>
2020-09-16 17:07:02 +0300

    hvlgtptun refactored

--- native/hvlgtptun/hvlgtptun.cpp
+++ native/hvlgtptun/hvlgtptun.cpp
@@ -24,9 +24,8 @@ static int tun_alloc(char *dev, int flags)
 {
     struct ifreq ifr;
     int fd, err;
-    const char *clonedev = "/dev/net/tun";
 
-    if ((fd = open(clonedev, O_RDWR)) < 0)
+    if ((fd = open("/dev/net/tun", O_RDWR)) < 0)
     {
         perror("Opening /dev/net/tun");
         return fd;
