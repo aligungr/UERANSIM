4764f6bbd2152ec728860d796cbcb05cf096f60d
aligungr <aligng1620@gmail.com>
2020-10-12 20:43:13 +0300

    GtpTask refactor

--- ueransim/src/main/java/tr/havelsan/ueransim/app/api/gnb/gtp/GtpTask.java
+++ ueransim/src/main/java/tr/havelsan/ueransim/app/api/gnb/gtp/GtpTask.java
@@ -4,6 +4,7 @@ import tr.havelsan.ueransim.app.itms.Itms;
 import tr.havelsan.ueransim.app.itms.ItmsTask;
 import tr.havelsan.ueransim.app.itms.wrappers.UplinkDataWrapper;
 import tr.havelsan.ueransim.app.structs.simctx.GnbSimContext;
+import tr.havelsan.ueransim.utils.octets.OctetString;
 
 public class GtpTask extends ItmsTask {
 
@@ -19,8 +20,11 @@ public class GtpTask extends ItmsTask {
         while (true) {
             var msg = itms.receiveMessage(this);
             if (msg instanceof UplinkDataWrapper) {
-
+                handleUplinkData(((UplinkDataWrapper) msg).ipData);
             }
         }
+    }
+
+    private void handleUplinkData(OctetString data) {
     }
 }
