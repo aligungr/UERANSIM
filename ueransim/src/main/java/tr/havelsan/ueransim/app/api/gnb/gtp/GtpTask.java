55262fab62364edffa41026abff92a03a205619c
aligungr <aligng1620@gmail.com>
2020-10-13 15:30:39 +0300

    GTP sending

--- ueransim/src/main/java/tr/havelsan/ueransim/app/api/gnb/gtp/GtpTask.java
+++ ueransim/src/main/java/tr/havelsan/ueransim/app/api/gnb/gtp/GtpTask.java
@@ -7,12 +7,18 @@ import tr.havelsan.ueransim.app.itms.wrappers.UplinkDataWrapper;
 import tr.havelsan.ueransim.app.structs.GtpUContext;
 import tr.havelsan.ueransim.app.structs.PduSessionResource;
 import tr.havelsan.ueransim.app.structs.simctx.GnbSimContext;
+import tr.havelsan.ueransim.gtp.GtpEncoder;
 import tr.havelsan.ueransim.gtp.GtpMessage;
 import tr.havelsan.ueransim.gtp.ext.PduSessionContainerExtHeader;
 import tr.havelsan.ueransim.gtp.pdusup.UlPduSessionInformation;
+import tr.havelsan.ueransim.utils.Tag;
 import tr.havelsan.ueransim.utils.bits.Bit6;
+import tr.havelsan.ueransim.utils.console.Log;
 import tr.havelsan.ueransim.utils.octets.Octet;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.util.ArrayList;
 
 // TODO: this class is curently POC.
@@ -21,6 +27,7 @@ public class GtpTask extends ItmsTask {
     private final GnbSimContext ctx;
     private GtpUContext gtpCtx;
     private PduSessionResource pduSession;
+    private DatagramSocket socket;
 
     public GtpTask(Itms itms, int taskId, GnbSimContext ctx) {
         super(itms, taskId);
@@ -30,6 +37,12 @@ public class GtpTask extends ItmsTask {
     @Override
     public void main() {
         this.gtpCtx = ctx.gtpUCtx = new GtpUContext();
+        try {
+            this.socket = new DatagramSocket(ctx.config.gtpPort, InetAddress.getByName(ctx.config.host));
+        } catch (Exception e) {
+            Log.error(Tag.CONNECTION, "Failed to bind GTP/UDP socket %s:%s (%s)", ctx.config.gtpPort, ctx.config.host, e.toString());
+            return;
+        }
 
         while (true) {
             var msg = itms.receiveMessage(this);
@@ -37,11 +50,11 @@ public class GtpTask extends ItmsTask {
                 var data = ((UplinkDataWrapper) msg).ipData;
                 if ((data.get(0) >> 4 & 0xF) != 4) {
                     // ignore non IPv4 packets
-                    return;
+                    break;
                 }
 
                 if (pduSession == null)
-                    return;
+                    break;
 
                 var gtp = new GtpMessage();
                 gtp.payload = data;
@@ -65,6 +78,12 @@ public class GtpTask extends ItmsTask {
     }
 
     private void sendGtp(GtpMessage msg, byte[] address) {
-
+        try {
+            var data = GtpEncoder.encode(msg);
+            var pck = new DatagramPacket(data, data.length, InetAddress.getByAddress(address), 2152);
+            socket.send(pck);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
