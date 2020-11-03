6d0ca4fc4db16d49a0d5b11a6e261b453367675d
aligungr <aligng1620@gmail.com>
2020-11-03 12:10:13 +0300

    StepperMessagingListener severity added

--- ueransim/src/main/java/tr/havelsan/ueransim/app/app/listeners/StepperMessagingListener.java
+++ ueransim/src/main/java/tr/havelsan/ueransim/app/app/listeners/StepperMessagingListener.java
@@ -5,6 +5,10 @@ import tr.havelsan.ueransim.app.app.AppConfig;
 import tr.havelsan.ueransim.app.common.simctx.BaseSimContext;
 import tr.havelsan.ueransim.app.common.sw.SwStep;
 import tr.havelsan.ueransim.app.utils.SocketWrapperSerializer;
+import tr.havelsan.ueransim.nas.core.messages.NasMessage;
+import tr.havelsan.ueransim.nas.impl.messages.*;
+import tr.havelsan.ueransim.ngap0.core.NGAP_BaseMessage;
+import tr.havelsan.ueransim.ngap0.core.NGAP_Value;
 import tr.havelsan.ueransim.utils.Json;
 import tr.havelsan.ueransim.utils.Severity;
 
@@ -12,14 +16,58 @@ public class StepperMessagingListener implements INodeMessagingListener {
 
     private WsConnectContext ws;
 
+    private static Severity messageSeverity(Object msg) {
+        if (msg instanceof NGAP_Value) {
+            if (msg instanceof NGAP_BaseMessage) {
+                var ngap = (NGAP_BaseMessage) msg;
+                switch (ngap.getPduType()) {
+                    case 1:
+                        return Severity.SUCCESS;
+                    case 2:
+                        return Severity.ERROR;
+                    default:
+                        return Severity.INFO;
+                }
+            } else {
+                return Severity.INFO;
+            }
+        } else if (msg instanceof NasMessage) {
+            return nasSeverity((NasMessage) msg);
+        } else {
+            return Severity.INFO;
+        }
+    }
+
+    private static Severity nasSeverity(NasMessage msg) {
+        if (msg instanceof AuthenticationFailure || msg instanceof AuthenticationReject || msg instanceof FiveGMmStatus
+                || msg instanceof FiveGSmStatus || msg instanceof PduSessionEstablishmentReject ||
+                msg instanceof PduSessionModificationReject || msg instanceof PduSessionReleaseReject ||
+                msg instanceof RegistrationReject || msg instanceof SecurityModeReject) {
+            return Severity.ERROR;
+        }
+
+        if (msg instanceof ConfigurationUpdateComplete || msg instanceof PduSessionAuthenticationComplete ||
+                msg instanceof PduSessionEstablishmentAccept || msg instanceof PduSessionModificationComplete
+                || msg instanceof PduSessionReleaseComplete || msg instanceof RegistrationAccept ||
+                msg instanceof RegistrationComplete || msg instanceof SecurityModeComplete
+                || msg instanceof ServiceAccept) {
+            return Severity.SUCCESS;
+        }
+
+        return Severity.INFO;
+    }
+
     private void onMessage(BaseSimContext ctx, Object message) {
         if (message == null) {
             return;
         }
 
         var loggerName = AppConfig.generateNodeName(ctx);
-        var severity = Severity.DEBUG; // TODO
+        var severity = messageSeverity(message);
         var messageName = message.getClass().getSimpleName();
+        if (messageName.startsWith("NGAP_"))
+            messageName = messageName.substring("NGAP_".length());
+
         var messageBody = Json.toJson(message);
 
         var swStep = new SwStep(loggerName, severity, messageName, messageBody);
