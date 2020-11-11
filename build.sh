43af8adb7b7e8ca21f29b9cfb34e4f88dc03e10a
aligungr <aligng1620@gmail.com>
2020-11-11 12:24:02 +0300

    build system refactored

--- build.sh
+++ build.sh
@@ -22,25 +22,33 @@
 # SOFTWARE.
 #
 
+
+################################################################################
 if [ -z "$JAVA_HOME" ]; then
 echo "ERROR: 'JAVA_HOME' is not defined" 1>&2;
 exit 1
 fi
-
-mvn clean compile
-
-cd native/ngap-native
-sh build_and_copy.sh
-cd ../..
-
-cd native/crypto-native
-sh build_and_copy.sh
-cd ../..
-
-cd native/app-native
-sh build_and_copy.sh
-cd ../..
-
-mvn package
+################################################################################
 mkdir -p build
-cp ueransim/target/ueransim-1.0.8.jar build/
+################################################################################
+cd native/ngap-native
+make
+cp libngap-native.so ../../build/
+cd ../..
+################################################################################
+cd native/crypto-native
+make
+cp libcrypto-native.so ../../build/
+cd ../..
+################################################################################
+cd native/app-native
+make
+cp libapp-native.so ../../build/
+cd ../..
+################################################################################
+#mvn clean compile
+################################################################################
+#mvn package
+#mkdir -p build
+#cp ueransim/target/ueransim-1.0.8.jar build/
+################################################################################
