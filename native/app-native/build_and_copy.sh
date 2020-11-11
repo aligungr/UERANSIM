13237625f6bbba43ba5f22860e5353ecc96462fd
aligungr <aligng1620@gmail.com>
2020-11-11 12:48:05 +0300

    build_and_copy.sh scripts deleted

--- native/app-native/build_and_copy.sh
+++
@@ -1,9 +0,0 @@
-if [ -z "$JAVA_HOME" ]; then
-echo "JAVA_HOME is not defined" 1>&2;
-exit 1
-fi
-
-make clean
-make
-mkdir -p ../../ueransim/src/main/resources
-cp -u libapp-native.so ../../ueransim/src/main/resources
