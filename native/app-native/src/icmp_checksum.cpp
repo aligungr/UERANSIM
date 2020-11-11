5e21baba1d9f3d616a7a306d300855c53d01464a
aligungr <aligng1620@gmail.com>
2020-11-11 16:27:36 +0300

    ICMP checksum calculation

---
+++ native/app-native/src/icmp_checksum.cpp
@@ -0,0 +1,43 @@
+#include "icmp_checksum.hpp"
+#include "utils.hpp"
+
+#include <cassert>
+
+static uint16_t icmp_checksum(uint16_t *icmph, size_t len)
+{
+    uint16_t ret = 0;
+    uint32_t sum = 0;
+    uint16_t odd_byte;
+
+    while (len > 1)
+    {
+        sum += *icmph++;
+        len -= 2;
+    }
+
+    if (len == 1)
+    {
+        *(uint8_t *)(&odd_byte) = *(uint8_t *)icmph;
+        sum += odd_byte;
+    }
+
+    sum = (sum >> 16) + (sum & 0xffff);
+    sum += (sum >> 16);
+    ret = ~sum;
+
+    return ret;
+}
+
+void icmp_checksum_assign(uint8_t *data, size_t size)
+{
+    assert(size % 2 == 0); // Normally valid but not implemented this case
+
+    data[2] = 0;
+    data[3] = 0;
+
+    uint16_t csum = icmp_checksum(reinterpret_cast<uint16_t*>(data), size);
+
+    // Assuming this machine is LE
+    data[2] = csum;
+    data[3] = csum >> 8;
+}
---
+++ native/app-native/src/icmp_checksum.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <cstdint>
+#include <cstdlib>
+
+void icmp_checksum_assign(uint8_t* data, size_t size);
