90f96deeb90132457e681daf9f95866967f27aac
aligungr <aligng1620@gmail.com>
2020-11-12 13:35:26 +0300

    MmRegistration message improvement

--- ueransim/src/main/java/tr/havelsan/ueransim/app/ue/mm/MmRegistration.java
+++ ueransim/src/main/java/tr/havelsan/ueransim/app/ue/mm/MmRegistration.java
@@ -146,7 +146,6 @@ public class MmRegistration {
 
         Runnable unhandledRejectCase = () -> {
             Log.error(Tag.NOT_IMPL_YET, "Registration rejected with unhandled MMCause: %s", cause.name());
-            throw new NotImplementedException(String.format("Registration rejected with unhandled MMCause: %s", cause.name()));
         };
 
         if (regType.equals(ERegistrationType.INITIAL_REGISTRATION)) {
