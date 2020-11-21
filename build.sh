96c02aea50941a5c3b34c3f6dc81d0118af27457
aligungr <aligng1620@gmail.com>
2020-11-21 23:57:10 +0300

    copyright update

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
+#
 # Copyright (c) 2020 ALİ GÜNGÖR (aligng1620@gmail.com)
 # This software and all associated files are licensed under GPL-3.0.
+#
 
 if [ -z "$JAVA_HOME" ]; then
 echo "ERROR: 'JAVA_HOME' is not defined" 1>&2;
