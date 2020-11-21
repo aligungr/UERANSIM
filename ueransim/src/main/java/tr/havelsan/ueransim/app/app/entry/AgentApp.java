88ac6139a99f62debc71ed00dbc77834e3ac644d
aligungr <aligng1620@gmail.com>
2020-11-21 18:33:02 +0300

    AgentApp.java introduced

---
+++ ueransim/src/main/java/tr/havelsan/ueransim/app/app/entry/AgentApp.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2020 ALİ GÜNGÖR (aligng1620@gmail.com)
+ * This software and all associated files are licensed under GPL-3.0.
+ */
+
+package tr.havelsan.ueransim.app.app.entry;
+
+import tr.havelsan.ueransim.app.app.AppBuilder;
+import tr.havelsan.ueransim.app.app.AppConfig;
+import tr.havelsan.ueransim.app.common.itms.IwUeTestCommand;
+import tr.havelsan.ueransim.app.common.testcmd.TestCmd_InitialRegistration;
+import tr.havelsan.ueransim.itms.ItmsId;
+import tr.havelsan.ueransim.nas.impl.enums.EFollowOnRequest;
+import tr.havelsan.ueransim.utils.Utils;
+
+public class AgentApp {
+
+    public static void main(String[] args) {
+        BaseApp.main(args);
+
+        var appConfig = new AppConfig();
+
+        var ueransim = new AppBuilder()
+                .build();
+
+        // TODO gnb conencted ama sctp kurulmadı ise
+
+        var ueId = ueransim.createUe(appConfig.createUeConfig());
+        var gnbId = ueransim.createGnb(appConfig.createGnbConfig());
+
+        Utils.sleep(2000);
+        ueransim.findUe(ueId).itms.sendMessage(ItmsId.UE_TASK_APP, new IwUeTestCommand(new TestCmd_InitialRegistration(EFollowOnRequest.FOR_PENDING)));
+    }
+}
