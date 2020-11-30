6c82c1b03795b83106e49c2f2e7e1f734be23981
aligungr <aligng1620@gmail.com>
2020-11-30 19:32:00 +0300

    RRC_Enumerated added

---
+++ rrc/src/main/java/tr/havelsan/ueransim/rrc/rrc2/core/RRC_Enumerated.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2020 ALİ GÜNGÖR (aligng1620@gmail.com)
+ * This software and all associated files are licensed under GPL-3.0.
+ */
+
+package tr.havelsan.ueransim.rrc.rrc2.core;
+
+public abstract class RRC_Enumerated extends RRC_Value {
+
+    public final String sValue;
+
+    protected RRC_Enumerated(String sValue) {
+        this.sValue = sValue;
+    }
+}
