bd3321a63c2242ff56a338d3941396e9ffed5495
aligungr <aligng1620@gmail.com>
2020-12-01 23:53:35 +0300

    RRC_Choice improvement

--- rrc/src/main/java/tr/havelsan/ueransim/rrc/asn/core/RRC_Choice.java
+++ rrc/src/main/java/tr/havelsan/ueransim/rrc/asn/core/RRC_Choice.java
@@ -11,6 +11,34 @@ public abstract class RRC_Choice extends RRC_Value {
 
     public abstract String[] getMemberIdentifiers();
 
+    public RRC_Value getPresentValue() {
+        var cls = getClass();
+        for (String id : getMemberIdentifiers()) {
+            try {
+                var field = cls.getField(id);
+                var value = field.get(this);
+                if (value != null) return (RRC_Value) value;
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return null;
+    }
+
+    public void setPresentValue(RRC_Value value) {
+        var cls = getClass();
+        for (String id : getMemberIdentifiers()) {
+            try {
+                var field = cls.getField(id);
+                if (field.getType() == value.getClass()) {
+                    field.set(this, value);
+                }
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
     @Override
     public String getAsnName() {
         return "CHOICE";
