22022fe656c71bc39b030d9ecb7ddbd1ea38582f
aligungr <aligng1620@gmail.com>
2020-12-01 21:44:26 +0300

    RRC_Sequence added

---
+++ rrc/src/main/java/tr/havelsan/ueransim/rrc/rrc2/core/RRC_Sequence.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2020 ALİ GÜNGÖR (aligng1620@gmail.com)
+ * This software and all associated files are licensed under GPL-3.0.
+ */
+
+package tr.havelsan.ueransim.rrc.rrc2.core;
+
+public class RRC_Sequence extends RRC_Value {
+
+    @Override
+    public String getAsnName() {
+        return "SEQUENCE";
+    }
+
+    @Override
+    public String getXmlTagName() {
+        return "SEQUENCE";
+    }
+}
