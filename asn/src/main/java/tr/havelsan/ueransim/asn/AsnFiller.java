5bd52d22f2b28488ca73b7cf1924723af68adc24
aligungr <aligng1620@gmail.com>
2020-12-05 20:41:58 +0300

    RRC ASN

---
+++ asn/src/main/java/tr/havelsan/ueransim/asn/AsnFiller.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2020 ALİ GÜNGÖR (aligng1620@gmail.com)
+ * This software and all associated files are licensed under GPL-3.0.
+ */
+
+package tr.havelsan.ueransim.asn;
+
+public class AsnFiller {
+    // TODO: random value filler
+}
