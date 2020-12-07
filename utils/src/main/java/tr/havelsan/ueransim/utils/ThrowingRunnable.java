ca4aec5c9aad2d8522f6b26c583f517e0889dd95
aligungr <aligng1620@gmail.com>
2020-12-07 12:21:26 +0300

    ThrowingRunnable impl.

---
+++ utils/src/main/java/tr/havelsan/ueransim/utils/ThrowingRunnable.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2020 ALİ GÜNGÖR (aligng1620@gmail.com)
+ * This software and all associated files are licensed under GPL-3.0.
+ */
+
+package tr.havelsan.ueransim.utils;
+
+@FunctionalInterface
+public interface ThrowingRunnable {
+    void run() throws Exception;
+}
