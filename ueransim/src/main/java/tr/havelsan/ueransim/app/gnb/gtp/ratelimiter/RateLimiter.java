215026d2eb3f0780496488f9c21b3fad10d97b1d
Daniele <daniele.rossi18@studio.unibo.it>
2020-12-09 20:34:29 +0100

    Add javadoc comments.

--- ueransim/src/main/java/tr/havelsan/ueransim/app/gnb/gtp/ratelimiter/RateLimiter.java
+++ ueransim/src/main/java/tr/havelsan/ueransim/app/gnb/gtp/ratelimiter/RateLimiter.java
@@ -3,7 +3,21 @@ package tr.havelsan.ueransim.app.gnb.gtp.ratelimiter;
 import tr.havelsan.ueransim.app.common.PduSessionResource;
 import tr.havelsan.ueransim.utils.octets.OctetString;
 
+/**
+ * Interface for RateLimiter.
+ */
 public interface RateLimiter {
+    /**
+     * Handles downlink IP packets.
+     * @param pduSession the PDU session of which packets must be managed
+     * @param ipPacket the single IP packet.
+     */
     void handleDownlinkPacket(PduSessionResource pduSession, OctetString ipPacket);
+
+    /**
+     * Handles uplink IP packets.
+     * @param pduSession the PDU session of which packets must be managed
+     * @param ipPacket the single IP packet.
+     */
     void handleUplinkPacket(PduSessionResource pduSession, OctetString ipPacket);
 }
--- ueransim/src/main/java/tr/havelsan/ueransim/app/gnb/gtp/ratelimiter/TokenBucket.java
+++ ueransim/src/main/java/tr/havelsan/ueransim/app/gnb/gtp/ratelimiter/TokenBucket.java
@@ -1,5 +1,11 @@
 package tr.havelsan.ueransim.app.gnb.gtp.ratelimiter;
 
+/**
+ * It represents a simple TokenBucket. In order to save resources is a passive entity: it computes refill tokens number
+ * based on the time elapsed between the call to the tryConsume method and the next. In this implementation a token
+ * corresponds to a Byte.
+ * https://github.com/vladimir-bukhtoyarov/bucket4j/blob/master/doc-pages/token-bucket-brief-overview.md
+ */
 public class TokenBucket {
 
     private final static long REFILL_PERIOD = 1000L;
@@ -9,14 +15,23 @@ public class TokenBucket {
     private double availableTokens;
     private long lastRefillTimestamp;
 
-
-    public TokenBucket(long capacity) {
-        this.capacity = capacity;
-        this.refillTokensPerOneMillis = (double) capacity / (double) REFILL_PERIOD;
-        this.availableTokens = capacity;
+    /**
+     * Constructor method.
+     * @param byteCapacity The capacity in Byte of the bucket (i.e the AMBR treshold).
+     */
+    public TokenBucket(long byteCapacity) {
+        this.capacity = byteCapacity;
+        this.refillTokensPerOneMillis = (double) byteCapacity / (double) REFILL_PERIOD;
+        this.availableTokens = byteCapacity;
         this.lastRefillTimestamp = System.currentTimeMillis();
     }
 
+
+    /**
+     * It tries to consume a specific number of tokens.
+     * @param numberTokens the number of tokens to consume.
+     * @return true if ther is enought tokens, false otherwise.
+     */
     public boolean tryConsume(int numberTokens) {
         refill();
         if (availableTokens < numberTokens) {
@@ -27,6 +42,9 @@ public class TokenBucket {
         }
     }
 
+    /*
+     * Computes refill tokens number.
+     */
     private void refill() {
         long currentTimeMillis = System.currentTimeMillis();
         if (currentTimeMillis > lastRefillTimestamp) {
