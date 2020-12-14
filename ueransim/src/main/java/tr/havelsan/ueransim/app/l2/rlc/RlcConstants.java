13e7fc31e43b8047f55970a102e7a28f82423154
aligungr <aligng1620@gmail.com>
2020-12-14 22:14:01 +0300

    UmdPdu decoding

---
+++ ueransim/src/main/java/tr/havelsan/ueransim/app/l2/rlc/RlcConstants.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2020 ALİ GÜNGÖR (aligng1620@gmail.com)
+ * This software and all associated files are licensed under GPL-3.0.
+ */
+
+package tr.havelsan.ueransim.app.l2.rlc;
+
+public class RlcConstants {
+
+    public static final int SI_FULL = 0b00;
+    public static final int SI_FIRST = 0b01;
+    public static final int SI_LAST = 0b10;
+    public static final int SI_MIDDLE = 0b11;
+
+}
--- ueransim/src/main/java/tr/havelsan/ueransim/app/l2/rlc/pdu/UmdPdu.java
+++ ueransim/src/main/java/tr/havelsan/ueransim/app/l2/rlc/pdu/UmdPdu.java
@@ -5,6 +5,8 @@
 
 package tr.havelsan.ueransim.app.l2.rlc.pdu;
 
+import tr.havelsan.ueransim.app.l2.rlc.RlcConstants;
+import tr.havelsan.ueransim.utils.OctetInputStream;
 import tr.havelsan.ueransim.utils.octets.OctetString;
 
 public class UmdPdu {
@@ -12,4 +14,28 @@ public class UmdPdu {
     public int so;
     public int sn;
     public OctetString data;
+
+    public static UmdPdu decode(OctetInputStream stream, boolean isShortSn) {
+        var octet = stream.readOctet();
+
+        var umd = new UmdPdu();
+        umd.si = octet.getBitRangeI(6, 7);
+
+        if (umd.si != RlcConstants.SI_FULL) {
+            if (isShortSn) {
+                umd.sn = octet.getBitRangeI(0, 5);
+            } else {
+                umd.sn = octet.getBitRangeI(0, 3);
+                umd.sn <<= 8;
+                umd.sn |= octet.getBitRangeI(0, 7);
+            }
+
+            if (umd.si != RlcConstants.SI_FIRST) {
+                umd.so = stream.readOctet2I();
+            }
+        }
+
+        umd.data = stream.readOctetString();
+        return umd;
+    }
 }
