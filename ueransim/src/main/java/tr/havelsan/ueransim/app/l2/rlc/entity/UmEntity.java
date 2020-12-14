7c89f9aef65c9586ad03111cf1266aecc2c3fd15
aligungr <aligng1620@gmail.com>
2020-12-14 22:45:19 +0300

    UmEntity improvement

--- ueransim/src/main/java/tr/havelsan/ueransim/app/l2/rlc/entity/UmEntity.java
+++ ueransim/src/main/java/tr/havelsan/ueransim/app/l2/rlc/entity/UmEntity.java
@@ -5,9 +5,31 @@
 
 package tr.havelsan.ueransim.app.l2.rlc.entity;
 
+import tr.havelsan.ueransim.app.l2.rlc.RlcConstants;
+import tr.havelsan.ueransim.app.l2.rlc.RlcTransfer;
+import tr.havelsan.ueransim.app.l2.rlc.pdu.UmdPdu;
+import tr.havelsan.ueransim.utils.OctetInputStream;
 import tr.havelsan.ueransim.utils.octets.OctetString;
 
 public class UmEntity extends RlcEntity {
+
+    private int snLength = 6;
+
+    private int snModulus;
+    private int windowSize;
+
+    private int rxNextReassembly; // Earliest SN that is still considered for reassembly
+    private int rxNextHighest; //
+
+    private int modulusRx(int num) {
+        int r = num - (rxNextHighest - windowSize);
+        if (r < 0) r += snModulus;
+        return r % snModulus;
+    }
+
+    private int snCompareRx(int a, int b) {
+        return modulusRx(a) - modulusRx(b);
+    }
 
     @Override
     public OctetString createPdu(OctetString sdu) {
@@ -16,7 +38,22 @@ public class UmEntity extends RlcEntity {
 
     @Override
     public void receivePdu(OctetString data) {
+        var pdu = UmdPdu.decode(new OctetInputStream(data), snLength == 6);
 
+        if (pdu.si == RlcConstants.SI_FULL) {
+            RlcTransfer.deliverSdu(this, pdu.data);
+            return;
+        }
+
+        if (pdu.data.length == 0) {
+            return;
+        }
+
+        if (snCompareRx(pdu.sn, rxNextReassembly) < 0) {
+            return;
+        }
+
+        // TODO place the received UMD PDU in the reception buffer
     }
 
     @Override
