64a68b2d030718b84f0a5b53316a77aaeb25f30d
aligungr <aligng1620@gmail.com>
2020-12-16 21:30:54 +0300

    NackBlock comment added

--- ueransim/src/main/java/tr/havelsan/ueransim/app/link/rlc/pdu/NackBlock.java
+++ ueransim/src/main/java/tr/havelsan/ueransim/app/link/rlc/pdu/NackBlock.java
@@ -3,6 +3,10 @@ package tr.havelsan.ueransim.app.link.rlc.pdu;
 public class NackBlock {
     public int nackSn;
     public int soStart;
+
+    // (The special SOend value "1111111111111111" is used to indicate that the missing portion
+    // of the RLC SDU includes all bytes to the last byte of the RLC SDU.)
     public int soEnd;
+
     public int nackRange;
 }
