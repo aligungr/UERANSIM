659d560969cfbad7f7655752ac71c2891662d8a0
aligungr <aligng1620@gmail.com>
2020-12-30 21:27:36 +0300

    RLC removed

--- ueransim/src/main/java/tr/havelsan/ueransim/app/link/mac/Mac.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2020 ALİ GÜNGÖR (aligng1620@gmail.com)
- * This software and all associated files are licensed under GPL-3.0.
- */
-
-package tr.havelsan.ueransim.app.link.mac;
-
-public class Mac {
-}
