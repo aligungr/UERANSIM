c508e5e4320e3401a87fc86243025dd0537dd38d
Alan Chang <alanchang@nycu.edu.tw>
2021-10-08 13:33:58 +0000

    fix null pointer exception

--- src/gnb/gtp/task.cpp
+++ src/gnb/gtp/task.cpp
@@ -135,12 +135,19 @@ void GtpTask::handleSessionRelease(int ueId, int psi)
         return;
     }
 
+
     uint64_t sessionInd = MakeSessionResInd(ueId, psi);
 
     // Remove all session information from rate limiter
     m_rateLimiter->updateSessionUplinkLimit(sessionInd, 0);
     m_rateLimiter->updateUeDownlinkLimit(ueId, 0);
 
+    
+    if (!m_pduSessions[sessionInd]->downTunnel) {
+         m_logger->err("PDU session resource could not be released, PSI downlink tunnel [%d] not found", psi);
+        return;
+    }
+    
     // And remove from PDU session table
     uint32_t teid = m_pduSessions[sessionInd]->downTunnel.teid;
     m_pduSessions.erase(sessionInd);
